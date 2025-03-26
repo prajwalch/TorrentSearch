4834828d3af2f485c42c84983c569f9065de61b1
prajwalch <prajwal.chapagain58@gmail.com>
2025-03-26 23:18:25 +0545

    fix: Make result list scrollable
    Signed-off-by: prajwalch <prajwal.chapagain58@gmail.com>

--- app/src/main/kotlin/com/prajwalch/torrentsearch/ui/screens/SearchScreen.kt
+++ app/src/main/kotlin/com/prajwalch/torrentsearch/ui/screens/SearchScreen.kt
@@ -8,6 +8,8 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.HorizontalDivider
@@ -37,8 +39,8 @@ fun SearchScreen(viewModel: SearchScreenViewModel) {
         CircularProgressIndicator()
     }
 
-    for (torrent in uiState.results) {
-        TorrentListItem(torrent, onClick = {})
+    LazyColumn {
+        items(uiState.results) { TorrentListItem(it, onClick = {}) }
     }
 }
 
