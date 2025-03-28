eb2eeea8b97bc3aff3e45dda5cdd1d366dff1440
prajwalch <prajwal.chapagain58@gmail.com>
2025-03-28 16:28:36 +0545

    refactor: Cleanup imports
    Signed-off-by: prajwalch <prajwal.chapagain58@gmail.com>

--- app/src/main/kotlin/com/prajwalch/torrentsearch/HttpClient.kt
+++ app/src/main/kotlin/com/prajwalch/torrentsearch/HttpClient.kt
@@ -4,6 +4,7 @@ import io.ktor.client.HttpClient
 import io.ktor.client.engine.cio.CIO
 import io.ktor.client.request.get
 import io.ktor.client.statement.bodyAsText
+
 import kotlinx.serialization.json.Json
 import kotlinx.serialization.json.JsonElement
 
