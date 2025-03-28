e79e51baca99504c5f217b48bbe591e0376539a7
prajwalch <prajwal.chapagain58@gmail.com>
2025-03-28 20:31:37 +0545

    feat: Improve/polish content type nav bar
    Signed-off-by: prajwalch <prajwal.chapagain58@gmail.com>

--- app/src/main/kotlin/com/prajwalch/torrentsearch/ui/screens/SearchScreen.kt
+++ app/src/main/kotlin/com/prajwalch/torrentsearch/ui/screens/SearchScreen.kt
@@ -3,18 +3,26 @@ package com.prajwalch.torrentsearch.ui.screens
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Clear
+import androidx.compose.material.icons.filled.Done
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.FilterChip
+import androidx.compose.material3.FilterChipDefaults
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -24,6 +32,7 @@ import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
@@ -40,6 +49,7 @@ fun SearchScreen(viewModel: SearchScreenViewModel, onTorrentSelect: (Torrent) ->
 
     SearchBox(uiState.query, onQueryChange = viewModel::setQuery, onSubmit = viewModel::onSubmit)
     ContentTypeNavBar(uiState.contentType, onSelect = viewModel::setContentType)
+    HorizontalDivider()
 
     if (uiState.isLoading) {
         CircularProgressIndicator()
@@ -79,23 +89,41 @@ fun SearchBox(query: String, onQueryChange: (String) -> Unit, onSubmit: () -> Un
 
 @Composable
 fun ContentTypeNavBar(activeContentType: ContentType, onSelect: (ContentType) -> Unit) {
-    // FIXME: Make it scrollable.
-    Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceAround) {
-        for (contentType in ContentType.entries) ContentTypeNavBarItem(
-            label = contentType.toString(),
-            isActive = activeContentType == contentType,
-            onClick = { onSelect(contentType) }
-        )
+    LazyRow(
+        modifier = Modifier.fillMaxWidth(),
+        contentPadding = PaddingValues(5.dp),
+        horizontalArrangement = Arrangement.Center,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        itemsIndexed(ContentType.entries.toList()) { index, contentType ->
+            // TODO: This is hack to add space between items.
+            //       horizontalArrangement is not working.
+            if (index != 0) {
+                Spacer(Modifier.width(10.dp))
+            }
+
+            ContentTypeNavBarItem(
+                label = contentType.toString(),
+                isActive = activeContentType == contentType,
+                onClick = { onSelect(contentType) }
+            )
+        }
     }
 }
 
 @Composable
 fun ContentTypeNavBarItem(label: String, isActive: Boolean, onClick: () -> Unit) {
-    Text(
-        text = label,
-        modifier = Modifier.clickable(onClick = onClick),
-        color = if (isActive) Color.Unspecified else Color.Gray,
-        fontSize = 14.sp
+    FilterChip(
+        selected = isActive,
+        onClick = onClick,
+        label = { Text(label) },
+        leadingIcon = {
+            if (isActive) Icon(
+                imageVector = Icons.Default.Done,
+                contentDescription = "Selected content type",
+                modifier = Modifier.size(FilterChipDefaults.IconSize)
+            )
+        },
     )
 }
 
