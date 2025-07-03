562aff95738421aab01d16002334c17926a4a659
prajwalch <prajwal.chapagain58@gmail.com>
2025-07-03 15:21:22 +0545

    refactor(ThePirateBay): Polish and document it properly
    Signed-off-by: prajwalch <prajwal.chapagain58@gmail.com>

--- app/src/main/kotlin/com/prajwalch/torrentsearch/providers/ThePirateBay.kt
+++ app/src/main/kotlin/com/prajwalch/torrentsearch/providers/ThePirateBay.kt
@@ -14,17 +14,27 @@ import kotlinx.serialization.json.jsonObject
 class ThePirateBay : SearchProvider {
     override suspend fun search(query: String, context: SearchContext): List<Torrent> {
         val categoryIndex = categoryIndex(context.category)
-        val requestUrl = "$URL?q=$query&cat=$categoryIndex"
+        val queryParams = "?q=$query&cat=$categoryIndex"
+        val requestUrl = "$URL$queryParams"
 
-        val response = context.httpClient.getJson(requestUrl)
-        val torrents = response
+        val responseArray = context
+            .httpClient
+            .getJson(requestUrl)
             ?.jsonArray
-            ?.map { arrayItem -> arrayItem.jsonObject }
-            ?.mapNotNull(::parseJsonObject)
+            ?: return emptyList()
+
+        val torrents = responseArray
+            .map { rawArrayItem -> rawArrayItem.jsonObject }
+            .mapNotNull { torrentObject -> parseTorrentObject(torrentObject = torrentObject) }
 
         return torrents.orEmpty()
     }
 
+    /**
+     * Returns the index of a given category.
+     *
+     * Visit https://thepiratebay.org/browse.php to see all the supported index.
+     */
     private fun categoryIndex(category: Category): UInt = when (category) {
         Category.All, Category.Anime -> 0u
         Category.Apps -> 300u
@@ -35,21 +45,37 @@ class ThePirateBay : SearchProvider {
         Category.Porn -> 500u
     }
 
-    private fun parseJsonObject(jsonObject: JsonObject): Torrent? {
-        val name = jsonObject["name"]?.toString()?.trim('"') ?: return null
+    /**
+     * Parses the torrent object and returns [Torrent] if the object has a
+     * expected layout, otherwise returns `null`.
+     *
+     * Object layout (only shown necessary fields).
+     *
+     *     name:               <string>
+     *     info_hash:          <string>
+     *     leechers:           <number>
+     *     seeders:            <number>
+     *     size:               <bytes in string>
+     *     added:              <seconds in string>
+     */
+    private fun parseTorrentObject(torrentObject: JsonObject): Torrent? {
+        val name = torrentObject["name"]?.toString()?.trim('"') ?: return null
 
         // Yeah, this is how it returns empty results.
         if (name == "No results returned") {
             return null
         }
 
-        val hash = jsonObject["info_hash"]?.toString()?.trim('"') ?: return null
-        val sizeBytes = jsonObject["size"]?.toString()?.trim('"') ?: return null
-        val seeds = jsonObject["seeders"]?.toString()?.trim('"')?.toUIntOrNull() ?: return null
-        val peers = jsonObject["leechers"]?.toString()?.trim('"')?.toUIntOrNull() ?: return null
+        val hash = torrentObject["info_hash"]?.toString()?.trim('"') ?: return null
+        val sizeBytes = torrentObject["size"]?.toString()?.trim('"') ?: return null
+        val seeds = torrentObject["seeders"]?.toString()?.trim('"')?.toUIntOrNull() ?: return null
+        val peers = torrentObject["leechers"]?.toString()?.trim('"')?.toUIntOrNull() ?: return null
 
-        val uploadDateEpochSeconds =
-            jsonObject["added"]?.toString()?.trim('"')?.toLongOrNull() ?: return null
+        val uploadDateEpochSeconds = torrentObject["added"]
+            ?.toString()
+            ?.trim('"')
+            ?.toLongOrNull()
+            ?: return null
         val uploadDate = prettyDate(uploadDateEpochSeconds)
 
         return Torrent(
