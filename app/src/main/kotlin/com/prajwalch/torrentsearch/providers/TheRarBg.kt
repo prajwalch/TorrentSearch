a4917912a8b53fd147b0b56209550e96c448c6e4
prajwalch <prajwal.chapagain58@gmail.com>
2025-07-03 16:12:21 +0545

    fix(TheRagBg): Handle error/failure cases properly + polish
    Signed-off-by: prajwalch <prajwal.chapagain58@gmail.com>

--- app/src/main/kotlin/com/prajwalch/torrentsearch/providers/TheRarBg.kt
+++ app/src/main/kotlin/com/prajwalch/torrentsearch/providers/TheRarBg.kt
@@ -35,10 +35,7 @@ private data class TableRowParsedResult(
 )
 
 class TheRarBg : SearchProvider {
-    override suspend fun search(
-        query: String,
-        context: SearchContext,
-    ): List<Torrent> = coroutineScope {
+    override suspend fun search(query: String, context: SearchContext): List<Torrent> {
         var requestUrl = "$BASE_URL/get-posts/keywords:$query"
 
         if (context.category != Category.All) {
@@ -47,24 +44,7 @@ class TheRarBg : SearchProvider {
         }
 
         val resultsPageHtml = context.httpClient.get(requestUrl)
-        val resultsTableBody = Jsoup
-            .parse(resultsPageHtml)
-            .selectFirst("table > tbody")
-            ?: return@coroutineScope emptyList()
-
-        resultsTableBody
-            .children()
-            .map { tr -> parseTableRow(tr = tr) }
-            .map { parsedResult ->
-                async {
-                    processTableRowParsedResult(
-                        httpClient = context.httpClient,
-                        parsedResult = parsedResult
-                    )
-                }
-            }
-            .awaitAll()
-            .filterNotNull()
+        return parseAndExtractTorrents(httpClient = context.httpClient, html = resultsPageHtml)
     }
 
     /** Returns the compatible category string. */
@@ -82,18 +62,55 @@ class TheRarBg : SearchProvider {
         }
     }
 
+    /** Parses the HTML and returns all the extracted torrents from it. */
+    private suspend fun parseAndExtractTorrents(
+        httpClient: HttpClient,
+        html: String,
+    ): List<Torrent> = coroutineScope {
+        // Results are presented in a <table>, which is the only one present
+        // in a entire document.
+        //
+        // For some time saving, we can directly grab the table body, where the
+        // results are listed.
+        val resultsTableBody = Jsoup
+            .parse(html)
+            .selectFirst("table > tbody")
+            ?: return@coroutineScope emptyList()
+
+        resultsTableBody
+            // Grab all the children i.e. <tr>.
+            .children()
+            // Parse it and extract all the available but required data from it.
+            .mapNotNull { tr -> parseTableRow(tr = tr) }
+            // The info hash is not presented in a table, we have to grab it
+            // from the details page. Therefore, extract it and construct `Torrent`.
+            .map { parsedResult ->
+                async {
+                    processParsedTableRow(httpClient = httpClient, parsedResult = parsedResult)
+                }
+            }
+            .awaitAll()
+            .filterNotNull()
+    }
+
     /** Parses and extracts the necessary information from the table row. */
-    private fun parseTableRow(tr: Element): TableRowParsedResult {
+    private fun parseTableRow(tr: Element): TableRowParsedResult? {
         assert(tr.hasClass("list-entry"))
 
-        val nameAnchorElement = tr.selectFirst("td:nth-child(2)")!!.selectFirst("a")!!
+        val nameAnchorElement = tr
+            .selectFirst("td:nth-child(2)")
+            ?.selectFirst("a")
+            ?: return null
         val detailsPath = nameAnchorElement.attr("href")
         val torrentName = nameAnchorElement.ownText()
 
-        val uploadDate = tr.selectFirst("td:nth-child(4)")!!.selectFirst("div")!!.ownText()
-        val size = tr.selectFirst("td:nth-child(6)")!!.ownText()
-        val seeds = tr.selectFirst("td:nth-child(7)")!!.ownText()
-        val peers = tr.selectFirst("td:nth-child(8)")!!.ownText()
+        val uploadDate = tr.selectFirst("td:nth-child(4)")
+            ?.selectFirst("div")
+            ?.ownText()
+            ?: return null
+        val size = tr.selectFirst("td:nth-child(6)")?.ownText() ?: return null
+        val seeds = tr.selectFirst("td:nth-child(7)")?.ownText() ?: return null
+        val peers = tr.selectFirst("td:nth-child(8)")?.ownText() ?: return null
 
         return TableRowParsedResult(
             name = torrentName,
@@ -106,20 +123,18 @@ class TheRarBg : SearchProvider {
     }
 
     /**
-     * Further processes the table row parsed result and returns the fully
+     * Further processes the parsed table row and returns the fully
      * constructed [Torrent] if process succeed.
      */
-    private suspend fun processTableRowParsedResult(
+    private suspend fun processParsedTableRow(
         httpClient: HttpClient,
         parsedResult: TableRowParsedResult,
     ): Torrent? {
         // 1. Get the info hash from the details page.
-        val detailsPageHtml = httpClient.get(url = parsedResult.detailsPageUrl)
-        val infoHash = Jsoup
-            .parse(detailsPageHtml)
-            .selectFirst(".info-hash-value")
-            ?.ownText()
-            ?: return null
+        val infoHash = extractInfoHash(
+            httpClient = httpClient,
+            detailsPageUrl = parsedResult.detailsPageUrl,
+        ) ?: return null
 
         // 2. Construct torrent data.
         val sizeValue = parsedResult.size.takeWhile { !it.isWhitespace() }.toFloat()
@@ -134,6 +149,12 @@ class TheRarBg : SearchProvider {
             providerName = NAME,
             uploadDate = parsedResult.uploadDate,
         )
+    }
+
+    /** Extracts and returns the info hash from the details page, if exists. */
+    private suspend fun extractInfoHash(httpClient: HttpClient, detailsPageUrl: String): String? {
+        val detailsPageHtml = httpClient.get(url = detailsPageUrl)
+        return Jsoup.parse(detailsPageHtml).selectFirst(".info-hash-value")?.ownText()
     }
 
     private companion object {
