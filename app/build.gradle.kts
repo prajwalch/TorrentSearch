9413f632b262420b86779ffb550b9ab43877e2d4
prajwalch <prajwal.chapagain58@gmail.com>
2025-07-05 20:55:00 +0545

    chore: Bump version number on build file
    Signed-off-by: prajwalch <prajwal.chapagain58@gmail.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.prajwalch.torrentsearch"
         minSdk = 25
         targetSdk = 36
-        versionCode = 1
-        versionName = "0.1.0"
+        versionCode = 2
+        versionName = "0.1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
