cb4ccefb1cb46e2318070d01eeefb47f083ddc18
prajwalch <prajwal.chapagain58@gmail.com>
2025-07-18 15:16:20 +0545

    refactor: Move settings related components to `Settings`
    Signed-off-by: prajwalch <prajwal.chapagain58@gmail.com>

--- app/src/main/kotlin/com/prajwalch/torrentsearch/ui/components/Settings.kt
+++ app/src/main/kotlin/com/prajwalch/torrentsearch/ui/components/Settings.kt
@@ -1,18 +1,91 @@
 package com.prajwalch.torrentsearch.ui.components
 
+import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Icon
 import androidx.compose.material3.ListItem
 import androidx.compose.material3.ListItemDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.dp
+import com.prajwalch.torrentsearch.R
+
+@Composable
+fun SettingsSectionTitle(
+    title: String,
+    modifier: Modifier = Modifier,
+    color: Color = MaterialTheme.colorScheme.primary,
+    style: TextStyle = MaterialTheme.typography.titleSmall,
+) {
+    Text(
+        modifier = modifier.padding(16.dp),
+        text = title,
+        color = color,
+        style = style,
+    )
+}
+
+@Composable
+fun SettingsItem(
+    @DrawableRes
+    leadingIconId: Int,
+    headline: String,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+    supportingContent: String? = null,
+    trailingContent: @Composable (() -> Unit)? = null,
+) {
+    ListItem(
+        modifier = Modifier.Companion
+            .clickable(onClick = onClick)
+            .then(modifier),
+        leadingContent = {
+            Icon(
+                painter = painterResource(leadingIconId),
+                contentDescription = headline,
+            )
+        },
+        headlineContent = { Text(text = headline) },
+        supportingContent = supportingContent?.let { { Text(text = it) } },
+        trailingContent = trailingContent,
+    )
+}
+
+@Composable
+fun SettingsDialog(
+    @StringRes
+    title: Int,
+    onDismissRequest: () -> Unit,
+    modifier: Modifier = Modifier.Companion,
+    content: @Composable () -> Unit,
+) {
+    AlertDialog(
+        modifier = modifier,
+        onDismissRequest = onDismissRequest,
+        dismissButton = {
+            TextButton(onClick = onDismissRequest) {
+                Text(stringResource(R.string.settings_dialog_button_cancel))
+            }
+        },
+        confirmButton = {},
+        title = { Text(text = stringResource(title)) },
+        text = content,
+    )
+}
 
 @Composable
 fun SettingsOptionMenu(
