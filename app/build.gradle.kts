9e0b1653f0ac5f39e8c2d72d2266adae85f6b522
prajwalch <prajwal.chapagain58@gmail.com>
2025-07-25 23:58:27 +0545

    Bump version to `v0.3.0`
    Signed-off-by: prajwalch <prajwal.chapagain58@gmail.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.prajwalch.torrentsearch"
         minSdk = 25
         targetSdk = 36
-        versionCode = 5
-        versionName = "0.2.0"
+        versionCode = 6
+        versionName = "0.3.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
