bf7d2b6130ad32730d047c2299d654df873a2554
prajwalch <prajwal.chapagain58@gmail.com>
2025-07-27 20:57:35 +0545

    refactor(SearchProviders): Polish it
    Signed-off-by: prajwalch <prajwal.chapagain58@gmail.com>

--- app/src/main/kotlin/com/prajwalch/torrentsearch/providers/SearchProviders.kt
+++ app/src/main/kotlin/com/prajwalch/torrentsearch/providers/SearchProviders.kt
@@ -1,7 +1,9 @@
 package com.prajwalch.torrentsearch.providers
 
+/** Search providers manager. */
 object SearchProviders {
-    private val enabledByDefault = setOf(
+    /** List of providers which are enabled by default. */
+    private val ENABLED_BY_DEFAULT = setOf(
         Eztv(id = "p1"),
         Knaben(id = "p2"),
         NyaaSi(id = "p3"),
@@ -9,7 +11,8 @@ object SearchProviders {
         Yts(id = "p5"),
     )
 
-    private val disabledByDefault = setOf(
+    /** List of providers which are disabled by default. */
+    private val DISABLED_BY_DEFAULT = setOf(
         AnimeTosho(id = "p6"),
         LimeTorrents(id = "p7"),
         MyPornClub(id = "p8"),
@@ -17,17 +20,27 @@ object SearchProviders {
         TheRarBg(id = "p10"),
     )
 
-    private val all = enabledByDefault.plus(disabledByDefault).sortedBy { it.info.name }
+    /** List of all providers. */
+    private val ALL = ENABLED_BY_DEFAULT.plus(DISABLED_BY_DEFAULT).sortedBy { it.info.name }
 
+    /**
+     * Returns a list containing instance of search provider associated with
+     * the given ids.
+     */
     fun get(ids: Set<SearchProviderId>): List<SearchProvider> {
-        return all.filter { ids.contains(it.info.id) }
+        return ALL.filter { ids.contains(it.info.id) }
     }
 
+    /**
+     * Returns a list containing the id of search providers which are enabled
+     * by default.
+     */
     fun enabledIds(): Set<SearchProviderId> {
-        return enabledByDefault.map { it.info.id }.toSet()
+        return ENABLED_BY_DEFAULT.map { it.info.id }.toSet()
     }
 
+    /** Returns a list containing info of all search providers. */
     fun infos(): List<SearchProviderInfo> {
-        return all.map { it.info }
+        return ALL.map { it.info }
     }
 }
