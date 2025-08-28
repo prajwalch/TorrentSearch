978b869aeb0e461b1c4e3740dcc3ede122dc9e9b
prajwalch <prajwal.chapagain58@gmail.com>
2025-08-28 17:13:54 +0545

    feat: Revamp torrent client not found dialog UI
    Signed-off-by: prajwalch <prajwal.chapagain58@gmail.com>

--- app/src/main/kotlin/com/prajwalch/torrentsearch/ui/components/TorrentClientNotFoundDialog.kt
+++ app/src/main/kotlin/com/prajwalch/torrentsearch/ui/components/TorrentClientNotFoundDialog.kt
@@ -1,87 +1,103 @@
 package com.prajwalch.torrentsearch.ui.components
 
+import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
-import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Info
 import androidx.compose.material3.AlertDialog
-import androidx.compose.material3.AlertDialogDefaults
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.ListItem
 import androidx.compose.material3.ListItemDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.RectangleShape
-import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.platform.LocalUriHandler
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.DialogProperties
+
 import com.prajwalch.torrentsearch.R
 
-private val recommendedClients = listOf(
+private val torrentClients = listOf(
     TorrentClient(
         name = R.string.libretorrent,
-        url = createPlayStoreUrl(packageId = "org.proninyaroslav.libretorrent"),
-        source = R.string.source_playstore,
+        isRecommended = true,
+        sources = listOf(
+            Source(
+                logo = R.drawable.googleplay,
+                url = createPlayStoreUrl(packageId = "org.proninyaroslav.libretorrent")
+            ),
+            Source(
+                logo = R.drawable.fdroid,
+                url = createFDroidUrl(packageId = "org.proninyaroslav.libretorrent")
+            ),
+        )
     ),
-    TorrentClient(
-        name = R.string.libretorrent,
-        url = createFDroidUrl(packageId = "org.proninyaroslav.libretorrent"),
-        source = R.string.source_fdroid,
-    ),
-)
-
-private val otherClients = listOf(
-    // Aria2App.
     TorrentClient(
         name = R.string.aria2,
-        url = createFDroidUrl(packageId = "com.gianlu.aria2app"),
-        source = R.string.source_fdroid
+        sources = listOf(
+            Source(
+                logo = R.drawable.fdroid,
+                url = createFDroidUrl(packageId = "com.gianlu.aria2app"),
+            ),
+        )
     ),
-    // Gopeed.
     TorrentClient(
         name = R.string.gopeed,
-        url = createGithubReleaseUrl(username = "GopeedLab", repo = "gopeed"),
-        source = R.string.source_github,
-    ),
-    // PikaTorrent.
-    TorrentClient(
-        name = R.string.pikatorrent,
-        url = createPlayStoreUrl(packageId = "com.pikatorrent.PikaTorrent"),
-        source = R.string.source_playstore,
+        sources = listOf(
+            Source(
+                logo = R.drawable.github,
+                url = createGithubReleaseUrl(username = "GopeedLab", repo = "gopeed"),
+            ),
+        )
     ),
     TorrentClient(
         name = R.string.pikatorrent,
-        url = createGithubReleaseUrl(username = "G-Ray", repo = "pikatorrent"),
-        source = R.string.source_github,
+        sources = listOf(
+            Source(
+                logo = R.drawable.googleplay,
+                url = createPlayStoreUrl(packageId = "com.pikatorrent.PikaTorrent"),
+            ),
+            Source(
+                logo = R.drawable.github,
+                url = createGithubReleaseUrl(username = "G-Ray", repo = "pikatorrent"),
+            ),
+        )
     ),
-    // TorrServe.
     TorrentClient(
         name = R.string.torrserve,
-        url = createFDroidUrl(packageId = "ru.yourok.torrserve"),
-        source = R.string.source_fdroid,
+        sources = listOf(
+            Source(
+                logo = R.drawable.fdroid,
+                url = createFDroidUrl(packageId = "ru.yourok.torrserve"),
+            ),
+        )
     ),
 )
 
 private data class TorrentClient(
-    /* Name of the client. */
     @param:StringRes
     val name: Int,
-    /** Where to get it. */
+    val isRecommended: Boolean = false,
+    val sources: List<Source> = emptyList(),
+)
+
+private data class Source(
+    @param:DrawableRes
+    val logo: Int,
     val url: String,
-    @param:StringRes
-    /** Where it is available. */
-    val source: Int,
 )
 
 @Composable
@@ -127,98 +143,55 @@ private fun DialogTitle(modifier: Modifier = Modifier) {
 
 @Composable
 private fun DialogContent(modifier: Modifier = Modifier) {
-    Column(modifier = modifier) {
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.spacedBy(20.dp),
+    ) {
         Text(stringResource(R.string.torrent_client_not_found_main_content))
-        TorrentClientList()
+        TorrentClientList(clients = torrentClients)
     }
 }
 
 @Composable
-private fun TorrentClientList() {
-    val listContainerColor = MaterialTheme.colorScheme.secondaryContainer
-    val listContentColor = MaterialTheme.colorScheme.onSecondaryContainer
-
-    ListSectionTitle(title = stringResource(R.string.section_title_recommended_client))
-    TorrentClientLinkList(
-        clients = recommendedClients,
-        backgroundColor = listContainerColor,
-        contentColor = listContentColor,
-    )
-
-    ListSectionTitle(title = stringResource(R.string.section_title_other_clients))
-    TorrentClientLinkList(
-        clients = otherClients,
-        backgroundColor = listContainerColor.copy(alpha = 0.5f),
-        contentColor = listContentColor,
-    )
-}
-
-@Composable
-private fun ListSectionTitle(title: String, modifier: Modifier = Modifier) {
-    val color = AlertDialogDefaults.textContentColor.copy(alpha = 0.8f)
-
-    Text(
-        modifier = modifier.padding(vertical = 8.dp),
-        text = title,
-        color = color,
-    )
-}
-
-@Composable
-private fun TorrentClientLinkList(
-    clients: List<TorrentClient>,
-    backgroundColor: Color,
-    contentColor: Color,
-    modifier: Modifier = Modifier,
-) {
-
-    val firstItemCornerShape = MaterialTheme.shapes.medium.copy(
-        bottomStart = CornerSize(0.dp),
-        bottomEnd = CornerSize(0.dp),
-    )
-    val lastItemCornerShape = MaterialTheme.shapes.medium.copy(
-        topStart = CornerSize(0.dp),
-        topEnd = CornerSize(0.dp),
-    )
-
-    clients.forEachIndexed { index, client ->
-        val cornerShape = if (index == 0) {
-            firstItemCornerShape
-        } else if (clients.lastIndex == index) {
-            lastItemCornerShape
-        } else {
-            null
+private fun TorrentClientList(clients: List<TorrentClient>, modifier: Modifier = Modifier) {
+    LazyColumn(
+        modifier = modifier,
+        verticalArrangement = Arrangement.spacedBy(4.dp),
+    ) {
+        items(items = clients) {
+            TorrentClientListItem(
+                modifier = modifier,
+                client = it,
+            )
         }
-
-        TorrentClientLinkListItem(
-            modifier = modifier,
-            client = client,
-            backgroundColor = backgroundColor,
-            shape = cornerShape,
-            contentColor = contentColor,
-        )
     }
 }
 
 @Composable
-private fun TorrentClientLinkListItem(
-    client: TorrentClient,
-    backgroundColor: Color,
-    contentColor: Color,
-    modifier: Modifier = Modifier,
-    shape: Shape? = null,
-) {
-    val uriHandler = LocalUriHandler.current
+private fun TorrentClientListItem(client: TorrentClient, modifier: Modifier = Modifier) {
+    val colorScheme = MaterialTheme.colorScheme
 
-    val listItemColors = ListItemDefaults.colors(
-        containerColor = backgroundColor,
-        headlineColor = contentColor,
-        trailingIconColor = contentColor,
+    val primaryColors = ListItemDefaults.colors(
+        containerColor = colorScheme.primaryContainer,
+        headlineColor = colorScheme.onPrimaryContainer,
+        supportingColor = colorScheme.onPrimaryContainer.copy(alpha = 0.6f),
+        trailingIconColor = colorScheme.onPrimaryContainer,
     )
+    val secondaryColors = ListItemDefaults.colors(
+        containerColor = colorScheme.secondaryContainer,
+        headlineColor = colorScheme.onSecondaryContainer,
+        trailingIconColor = colorScheme.onSecondaryContainer,
+    )
+    val listItemColors = if (client.isRecommended) {
+        primaryColors
+    } else {
+        secondaryColors
+    }
 
-    DialogListItem(
+    ListItem(
         modifier = Modifier
-            .clickable { uriHandler.openUri(client.url) }
+            .clip(shape = MaterialTheme.shapes.medium)
+            .height(56.dp)
             .then(modifier),
         headlineContent = {
             Text(
@@ -227,26 +200,37 @@ private fun TorrentClientLinkListItem(
             )
         },
         supportingContent = {
-            Text(
-                text = stringResource(client.source),
-                style = MaterialTheme.typography.bodySmall
-            )
+            if (client.isRecommended) {
+                Text(
+                    text = stringResource(R.string.recommended_client)
+                )
+            }
         },
-        trailingContent = {
-            Icon(
-                modifier = Modifier.size(18.dp),
-                painter = painterResource(R.drawable.ic_open_in_new),
-                contentDescription = null,
-            )
-        },
+        trailingContent = { TorrentClientSourcesRow(sources = client.sources) },
         colors = listItemColors,
-        shape = shape ?: RectangleShape,
     )
+}
+
+@Composable
+private fun TorrentClientSourcesRow(sources: List<Source>, modifier: Modifier = Modifier) {
+    val uriHandler = LocalUriHandler.current
+
+    Row(modifier = modifier) {
+        sources.forEach {
+            IconButton(onClick = { uriHandler.openUri(it.url) }) {
+                Icon(
+                    painter = painterResource(it.logo),
+                    contentDescription = null,
+                )
+            }
+        }
+    }
 }
 
 private fun createFDroidUrl(packageId: String): String {
     return "https://f-droid.org/en/packages/$packageId"
 }
+
 
 private fun createGithubReleaseUrl(username: String, repo: String): String {
     return "https://github.com/$username/$repo/releases"
