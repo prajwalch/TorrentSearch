prajwalch/TorrentSearch

commit 02b96166e97b9f386047340151bdc86877606141

refactor: Leverage DI using hilt
Signed-off-by: prajwalch <prajwal.chapagain58@gmail.com>

app/src/main/kotlin/com/prajwalch/torrentsearch/ui/screens/SearchScreen.kt
@@ -45,6 +45,7 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.zIndex
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 
 import com.prajwalch.torrentsearch.R
@@ -72,11 +73,11 @@ import kotlinx.coroutines.launch
 fun SearchScreen(
     onNavigateToBookmarks: () -> Unit,
     onNavigateToSettings: () -> Unit,
-    viewModel: SearchViewModel,
     onTorrentSelect: (Torrent) -> Unit,
     snackbarHostState: SnackbarHostState,
     modifier: Modifier = Modifier,
 ) {
+    val viewModel = hiltViewModel<SearchViewModel>()
     val uiState by viewModel.uiState.collectAsStateWithLifecycle()
 
     val coroutineScope = rememberCoroutineScope()

app/build.gradle.kts
@@ -6,6 +6,7 @@ plugins {
     alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.ksp)
     alias(libs.plugins.androidx.room)
+    alias(libs.plugins.hilt)
 }
 
 android {
@@ -79,13 +80,17 @@ dependencies {
     implementation(libs.androidx.ui)
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
+    implementation(libs.hilt.android)
     implementation(libs.jsoup)
     implementation(libs.kotlinx.serialization.json)
     implementation(libs.ktor.client.core)
     implementation(libs.ktor.client.okhttp)
     implementation(libs.lazycolumnscrollbar)
     implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.hilt.navigation.compose)
+    
     ksp(libs.androidx.room.compiler)
+    ksp(libs.hilt.android.compiler)
 
     testImplementation(libs.junit)
 

app/src/main/kotlin/com/prajwalch/torrentsearch/TorrentSearch.kt
@@ -2,7 +2,9 @@ package com.prajwalch.torrentsearch
 
 import android.app.Application
 import com.prajwalch.torrentsearch.network.HttpClient
+import dagger.hilt.android.HiltAndroidApp
 
+@HiltAndroidApp
 class TorrentSearch : Application() {
     override fun onTerminate() {
         super.onTerminate()

app/src/main/kotlin/com/prajwalch/torrentsearch/data/repository/SearchHistoryRepository.kt
@@ -4,8 +4,9 @@ import com.prajwalch.torrentsearch.data.database.dao.SearchHistoryDao
 import com.prajwalch.torrentsearch.data.database.entities.SearchHistory
 
 import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
 
-class SearchHistoryRepository(private val dao: SearchHistoryDao) {
+class SearchHistoryRepository @Inject constructor(private val dao: SearchHistoryDao) {
     /**
      * Adds the given search history otherwise does noting if the query
      * is already saved.

app/src/main/kotlin/com/prajwalch/torrentsearch/data/repository/SearchProvidersRepository.kt
@@ -26,7 +26,9 @@ import com.prajwalch.torrentsearch.providers.Yts
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
 
-class SearchProvidersRepository(private val dao: TorznabSearchProviderDao) {
+import javax.inject.Inject
+
+class SearchProvidersRepository @Inject constructor(private val dao: TorznabSearchProviderDao) {
     /**
      * Instances of built-in search providers.
      *

app/src/main/kotlin/com/prajwalch/torrentsearch/data/repository/SettingsRepository.kt
@@ -14,7 +14,9 @@ import com.prajwalch.torrentsearch.providers.SearchProviderId
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
 
-class SettingsRepository(
+import javax.inject.Inject
+
+class SettingsRepository @Inject constructor(
     private val dataStore: DataStore<Preferences>,
     searchProvidersRepository: SearchProvidersRepository,
 ) {

app/src/main/kotlin/com/prajwalch/torrentsearch/data/repository/TorrentsRepository.kt
@@ -19,7 +19,9 @@ import kotlinx.coroutines.flow.channelFlow
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.launch
 
-class TorrentsRepository(
+import javax.inject.Inject
+
+class TorrentsRepository @Inject constructor(
     /** Uses for remote search. */
     private val httpClient: HttpClient,
     /** Uses for bookmarking torrents. */

app/src/main/kotlin/com/prajwalch/torrentsearch/di/DataStoreModule.kt
@@ -1,0 +1,25 @@
+package com.prajwalch.torrentsearch.di
+
+import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.preferencesDataStore
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+
+private val Context.settingsDataStore: DataStore<Preferences> by preferencesDataStore(
+    name = "settings"
+)
+
+@Module
+@InstallIn(SingletonComponent::class)
+object DataStoreModule {
+    @Provides
+    fun provideSettingsDataStore(
+        @ApplicationContext context: Context,
+    ): DataStore<Preferences> = context.settingsDataStore
+}

commit b6b280ff62c561fac49458a878d37f1f236efdc0

fix: Regression of 2737082 introduced in 02b9616
Signed-off-by: prajwalch <prajwal.chapagain58@gmail.com>

app/src/main/kotlin/com/prajwalch/torrentsearch/ui/screens/SearchScreen.kt
@@ -41,11 +41,13 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.zIndex
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.ViewModelStoreOwner
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 
 import com.prajwalch.torrentsearch.R
@@ -77,7 +79,9 @@ fun SearchScreen(
     snackbarHostState: SnackbarHostState,
     modifier: Modifier = Modifier,
 ) {
-    val viewModel = hiltViewModel<SearchViewModel>()
+    val context = LocalContext.current as ViewModelStoreOwner
+
+    val viewModel = hiltViewModel<SearchViewModel>(viewModelStoreOwner = context)
     val uiState by viewModel.uiState.collectAsStateWithLifecycle()
 
     val coroutineScope = rememberCoroutineScope()