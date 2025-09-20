95e32b3162c48a71f3fd9df5b89ff7387f93b926
prajwalch <prajwal.chapagain58@gmail.com>
2025-09-20 21:33:24 +0545

    fix: Regression of 4ff63cd69a9d94a368c61127cc7226dfefda9a17
    Signed-off-by: prajwalch <prajwal.chapagain58@gmail.com>

--- app/src/main/kotlin/com/prajwalch/torrentsearch/ui/search/SearchScreen.kt
+++ app/src/main/kotlin/com/prajwalch/torrentsearch/ui/search/SearchScreen.kt
@@ -4,6 +4,7 @@ import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.focusable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -75,7 +76,7 @@ fun SearchScreen(
             }
         },
     ) { innerPadding ->
-        Box {
+        Box(modifier = Modifier.focusable()) {
             SearchScreenContent(
                 modifier = Modifier
                     .fillMaxSize()
