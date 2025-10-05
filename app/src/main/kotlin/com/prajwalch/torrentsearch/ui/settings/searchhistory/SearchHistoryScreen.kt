30cd464ff418aa53144e29263ca35abb52f1cd69
prajwalch <prajwal.chapagain58@gmail.com>
2025-10-05 17:00:59 +0545

    refactor: Remove `SearchHistoryScreenTopBar()`
    Signed-off-by: prajwalch <prajwal.chapagain58@gmail.com>

--- app/src/main/kotlin/com/prajwalch/torrentsearch/ui/settings/searchhistory/SearchHistoryScreen.kt
+++ app/src/main/kotlin/com/prajwalch/torrentsearch/ui/settings/searchhistory/SearchHistoryScreen.kt
@@ -1,12 +1,9 @@
 package com.prajwalch.torrentsearch.ui.settings.searchhistory
 
-import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.layout.consumeWindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
@@ -15,7 +12,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
@@ -58,11 +54,6 @@ fun SearchHistoryScreen(onNavigateBack: () -> Unit, modifier: Modifier = Modifie
                     }
                 }
             )
-            SearchHistoryScreenTopBar(
-                onNavigateBack = onNavigateBack,
-                onDeleteAll = viewModel::deleteAllSearchHistory,
-                showDeleteAllAction = searchHistoryList.isNotEmpty(),
-            )
         },
     ) { innerPadding ->
         if (searchHistoryList.isEmpty()) {
@@ -88,36 +79,4 @@ fun SearchHistoryScreen(onNavigateBack: () -> Unit, modifier: Modifier = Modifie
             )
         }
     }
-}
-
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-private fun SearchHistoryScreenTopBar(
-    onNavigateBack: () -> Unit,
-    onDeleteAll: () -> Unit,
-    showDeleteAllAction: Boolean,
-    modifier: Modifier = Modifier,
-) {
-    TopAppBar(
-        modifier = modifier,
-        navigationIcon = {
-            ArrowBackIconButton(
-                onClick = onNavigateBack,
-                contentDescription = R.string.button_go_to_settings_screen,
-            )
-        },
-        title = { Text(text = stringResource(R.string.search_history_screen_title)) },
-        actions = {
-            AnimatedVisibility(visible = showDeleteAllAction) {
-                IconButton(onClick = onDeleteAll) {
-                    Icon(
-                        painter = painterResource(R.drawable.ic_delete_history),
-                        contentDescription = stringResource(
-                            R.string.desc_delete_all_search_history
-                        ),
-                    )
-                }
-            }
-        }
-    )
 }
