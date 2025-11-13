c1c5cb7839588c6bad10d1f0bb36a5a80b54abd6
prajwalch <prajwal.chapagain58@gmail.com>
2025-11-13 19:44:10 +0545

    feat(SearchHistoryScreen): Add delete all confirmation step
    Signed-off-by: prajwalch <prajwal.chapagain58@gmail.com>

--- app/src/main/kotlin/com/prajwalch/torrentsearch/ui/searchhistory/SearchHistoryScreen.kt
+++ app/src/main/kotlin/com/prajwalch/torrentsearch/ui/searchhistory/SearchHistoryScreen.kt
@@ -6,20 +6,26 @@ import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.consumeWindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.platform.LocalClipboard
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
@@ -54,6 +60,17 @@ fun SearchHistoryScreen(
     val coroutineScope = rememberCoroutineScope()
     val queryCopiedMessage = stringResource(R.string.search_history_query_copied_message)
 
+    var showDeleteAllConfirmationDialog by remember { mutableStateOf(false) }
+    if (showDeleteAllConfirmationDialog) {
+        DeleteAllConfirmationDialog(
+            onDismiss = { showDeleteAllConfirmationDialog = false },
+            onConfirm = {
+                viewModel.deleteAllSearchHistory()
+                showDeleteAllConfirmationDialog = false
+            },
+        )
+    }
+
     Scaffold(
         modifier = Modifier
             .fillMaxSize()
@@ -66,7 +83,7 @@ fun SearchHistoryScreen(
                 actions = {
                     if (searchHistoryList.isNotEmpty()) {
                         DeleteForeverIconButton(
-                            onClick = { viewModel.deleteAllSearchHistory() },
+                            onClick = { showDeleteAllConfirmationDialog = true },
                             contentDescription = R.string.search_history_action_delete_all,
                         )
                     }
@@ -148,5 +165,39 @@ private fun SearchHistoryListItem(
             .then(modifier),
         query = searchHistory.query,
         onDeleteClick = { onDelete(searchHistory.id) },
+    )
+}
+
+@Composable
+private fun DeleteAllConfirmationDialog(
+    onDismiss: () -> Unit,
+    onConfirm: () -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    AlertDialog(
+        modifier = modifier,
+        onDismissRequest = onDismiss,
+        icon = {
+            Icon(
+                painter = painterResource(R.drawable.ic_delete_forever),
+                contentDescription = null,
+            )
+        },
+        title = {
+            Text(text = stringResource(R.string.search_history_dialog_title_clear_history))
+        },
+        text = {
+            Text(text = stringResource(R.string.search_history_dialog_message_clear_history))
+        },
+        confirmButton = {
+            TextButton(onClick = onConfirm) {
+                Text(text = stringResource(R.string.search_history_button_clear))
+            }
+        },
+        dismissButton = {
+            TextButton(onClick = onDismiss) {
+                Text(text = stringResource(R.string.button_cancel))
+            }
+        },
     )
 }
