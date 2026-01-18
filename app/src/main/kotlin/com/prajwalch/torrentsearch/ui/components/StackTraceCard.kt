d4ba414c1ee654182dda777d244261faef33bc66
prajwalch <prajwal.chapagain58@gmail.com>
2026-01-18 22:18:05 +0545

    feat(StackTraceCard): Make flexible for customization
    Signed-off-by: prajwalch <prajwal.chapagain58@gmail.com>

--- app/src/main/kotlin/com/prajwalch/torrentsearch/ui/components/StackTraceCard.kt
+++ app/src/main/kotlin/com/prajwalch/torrentsearch/ui/components/StackTraceCard.kt
@@ -7,10 +7,13 @@ import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.selection.SelectionContainer
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardColors
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Shape
 
 import com.prajwalch.torrentsearch.ui.theme.spaces
 
@@ -18,11 +21,17 @@ import com.prajwalch.torrentsearch.ui.theme.spaces
 fun StackTraceCard(
     stackTrace: String,
     modifier: Modifier = Modifier,
+    shape: Shape = CardDefaults.shape,
+    colors: CardColors = CardDefaults.cardColors(),
     contentPadding: PaddingValues = PaddingValues(
         all = MaterialTheme.spaces.large,
     ),
 ) {
-    Card(modifier = modifier) {
+    Card(
+        modifier = modifier,
+        shape = shape,
+        colors = colors,
+    ) {
         Box(
             modifier = Modifier
                 .padding(contentPadding)
