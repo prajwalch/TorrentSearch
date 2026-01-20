c5273711e6bf8040025cee5f091eca8f3e199ba0
prajwalch <prajwal.chapagain58@gmail.com>
2026-01-20 18:26:57 +0545

    build: Migrate to JVM/Java to `17`
    Signed-off-by: prajwalch <prajwal.chapagain58@gmail.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,3 @@
-import org.jetbrains.kotlin.gradle.dsl.JvmTarget
-
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
@@ -41,13 +39,13 @@ android {
     }
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
 
     kotlin {
         compilerOptions {
-            jvmTarget.set(JvmTarget.JVM_11)
+            jvmToolchain(17)
         }
     }
 
