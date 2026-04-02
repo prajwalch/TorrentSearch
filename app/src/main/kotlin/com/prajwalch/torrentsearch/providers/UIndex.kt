6a4e05b83b2a3ab759e66ab052fb2b8b82569273
prajwalch <prajwal.chapagain58@gmail.com>
2026-04-02 20:39:54 +0545

    feat(UIndex): Add implementation for parsing details page
    Signed-off-by: prajwalch <prajwal.chapagain58@gmail.com>

--- app/src/main/kotlin/com/prajwalch/torrentsearch/providers/UIndex.kt
+++ app/src/main/kotlin/com/prajwalch/torrentsearch/providers/UIndex.kt
@@ -3,6 +3,8 @@ package com.prajwalch.torrentsearch.providers
 import com.prajwalch.torrentsearch.domain.model.Category
 import com.prajwalch.torrentsearch.domain.model.InfoHashOrMagnetUri
 import com.prajwalch.torrentsearch.domain.model.Torrent
+import com.prajwalch.torrentsearch.domain.model.TorrentDetails
+import com.prajwalch.torrentsearch.network.HttpClient
 
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
@@ -20,39 +22,49 @@ class UIndex : SearchProvider {
         enabledByDefault = true,
     )
 
+    private val categoryMap = mapOf(
+        Category.All to 0,
+        Category.Books to 0,
+        Category.Anime to 7,
+        Category.Apps to 5,
+        Category.Games to 3,
+        Category.Movies to 1,
+        Category.Music to 4,
+        Category.Porn to 6,
+        Category.Series to 2,
+        Category.Other to 8,
+    )
+
+    private val resultsPageParser = UIndexResultsPageParser(
+        providerName = info.name,
+        baseUrl = info.url,
+    )
+
     override suspend fun search(query: String, context: SearchContext): List<Torrent> {
         val requestUrl = buildString {
             append(info.url)
             append("/search.php")
             append("?search=$query")
-
-            val categoryIndex = getCategoryIndex(category = context.category)
-            append("&c=$categoryIndex")
+            append("&c=${categoryMap[context.category]}")
         }
 
         val responseHtml = context.httpClient.get(url = requestUrl)
-        val torrents = withContext(Dispatchers.Default) {
-            parseHtml(html = responseHtml)
-        }
-
-        return torrents.orEmpty()
+        return resultsPageParser.parse(responseHtml).orEmpty()
     }
 
-    private fun getCategoryIndex(category: Category): Int = when (category) {
-        Category.All, Category.Books -> 0
-        Category.Anime -> 7
-        Category.Apps -> 5
-        Category.Games -> 3
-        Category.Movies -> 1
-        Category.Music -> 4
-        Category.Porn -> 6
-        Category.Series -> 2
-        Category.Other -> 8
+    override suspend fun getDetails(detailsPageUrl: String): TorrentDetails? {
+        val responseHtml = HttpClient.get(detailsPageUrl)
+        return UIndexDetailsPageParser.parse(responseHtml, detailsPageUrl)
     }
+}
 
-    private fun parseHtml(html: String): List<Torrent>? {
-        return Jsoup
-            .parse(html)
+private class UIndexResultsPageParser(
+    private val providerName: String,
+    private val baseUrl: String,
+) {
+    suspend fun parse(responseHtml: String): List<Torrent>? = withContext(Dispatchers.Default) {
+        Jsoup
+            .parse(responseHtml)
             .selectFirst("table.sr-table > tbody")
             ?.children()
             ?.mapNotNull { parseTableRow(tr = it) }
@@ -72,7 +84,7 @@ class UIndex : SearchProvider {
         // Anchor which contains a name and description page URL.
         val nameHref = secondTd.selectFirst("a.sr-torrent-link") ?: return null
         val torrentName = nameHref.ownText()
-        val descriptionPageUrl = info.url + nameHref.attr("href")
+        val descriptionPageUrl = baseUrl + nameHref.attr("href")
 
         val size = tr.selectFirst("td.sr-col-size")?.ownText() ?: return null
         val uploadDate = tr.selectFirst("td.sr-col-uploaded")?.ownText() ?: return null
@@ -94,7 +106,7 @@ class UIndex : SearchProvider {
             size = size,
             seeders = seeders.toUIntOrNull() ?: 0u,
             peers = peers.toUIntOrNull() ?: 0u,
-            providerName = info.name,
+            providerName = providerName,
             uploadDate = uploadDate,
             category = category,
             descriptionPageUrl = descriptionPageUrl,
@@ -113,4 +125,54 @@ class UIndex : SearchProvider {
         "Other" -> Category.Other
         else -> Category.Other
     }
+}
+
+private object UIndexDetailsPageParser {
+    private const val NAME = ".dt-title"
+    private const val SIZE =
+        "#content > div.dt-info-card > div > div:nth-child(1) > div:nth-child(2) > span.dt-info-value"
+    private const val SEEDERS = ".dt-seed"
+    private const val PEERS = ".dt-leech"
+    private const val UPLOAD_DATE =
+        "#content > div.dt-info-card > div > div:nth-child(1) > div:nth-child(3) > span.dt-info-value"
+    private const val CATEGORY = ".sr-cat-badge"
+    private const val PEERS_UPDATED =
+        "#content > div.dt-info-card > div > div:nth-child(2) > div:nth-child(3) > span.dt-info-value"
+    private const val DESCRIPTION = ".dt-descr-body"
+    private const val THUMBNAIL = ".tmdb-poster > img"
+    private const val PREVIEW_IMAGE = "img.torrent-img"
+    private const val MAGNET_URI = "a.dt-download-btn"
+
+    suspend fun parse(responseHtml: String, baseUrl: String): TorrentDetails? =
+        withContext(Dispatchers.Default) {
+            val html = Jsoup.parse(responseHtml, baseUrl)
+
+            // Required data. Return early as possible.
+            val name = html.selectFirst(NAME)?.ownText() ?: return@withContext null
+            val magnetUri = html.selectFirst(MAGNET_URI)?.attr("href") ?: return@withContext null
+
+            val size = html.selectFirst(SIZE)?.ownText() ?: "0 KB"
+            val seeders = html.selectFirst(SEEDERS)?.ownText()?.toUIntOrNull() ?: 1U
+            val peers = html.selectFirst(PEERS)?.ownText()?.toUIntOrNull() ?: 1U
+            val uploadDate = html.selectFirst(UPLOAD_DATE)?.ownText() ?: "0 min ago"
+            val category = html.selectFirst(CATEGORY)?.ownText()
+            val lastChecked = html.selectFirst(PEERS_UPDATED)?.text()?.takeIf { it.isNotBlank() }
+            val description = html.selectFirst(DESCRIPTION)?.html()
+            val thumbnailUrl = html.selectFirst(THUMBNAIL)?.attr("abs:src")
+            val previewImageUrls = html.select(PREVIEW_IMAGE).map { it.attr("src") }
+
+            TorrentDetails(
+                name = name,
+                size = size,
+                seeders = seeders,
+                peers = peers,
+                uploadDate = uploadDate,
+                category = category,
+                lastChecked = lastChecked,
+                description = description,
+                posterUrl = thumbnailUrl,
+                infoHashOrMagnetUri = InfoHashOrMagnetUri.MagnetUri(magnetUri),
+                screenshotUrls = previewImageUrls,
+            )
+        }
 }
