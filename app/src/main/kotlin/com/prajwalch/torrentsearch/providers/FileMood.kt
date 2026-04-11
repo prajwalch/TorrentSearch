6cebffe993bc040e39fa651815449017ed3ccd7c
prajwalch <prajwal.chapagain58@gmail.com>
2026-04-11 10:55:22 +0545

    feat(FileMood): Add support for details page
    Signed-off-by: prajwalch <prajwal.chapagain58@gmail.com>

--- app/src/main/kotlin/com/prajwalch/torrentsearch/providers/FileMood.kt
+++ app/src/main/kotlin/com/prajwalch/torrentsearch/providers/FileMood.kt
@@ -2,6 +2,9 @@ package com.prajwalch.torrentsearch.providers
 
 import com.prajwalch.torrentsearch.domain.model.Category
 import com.prajwalch.torrentsearch.domain.model.Torrent
+import com.prajwalch.torrentsearch.domain.model.TorrentDetails
+import com.prajwalch.torrentsearch.network.HttpClient
+import com.prajwalch.torrentsearch.util.TorrentUtils
 
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
@@ -19,6 +22,8 @@ class FileMood : SearchProvider {
         enabledByDefault = false,
     )
 
+    private val resultsPageParser = FileMoodResultsPageParser(info.name)
+
     override suspend fun search(query: String, context: SearchContext): List<Torrent> {
         val requestUrl = buildString {
             append(info.url)
@@ -27,18 +32,23 @@ class FileMood : SearchProvider {
             append("+in%3Atitle")
         }
         val responseHtml = context.httpClient.get(url = requestUrl)
+        return resultsPageParser.parse(html = responseHtml, pageUrl = requestUrl)
+    }
 
-        return withContext(Dispatchers.Default) {
-            parseResponseHtml(html = responseHtml)
+    override suspend fun getDetails(detailsPageUrl: String): TorrentDetails? {
+        val responseHtml = HttpClient.get(detailsPageUrl)
+        return FileMoodDetailsPageParser.parse(responseHtml)
+    }
+}
+
+private class FileMoodResultsPageParser(private val providerName: String) {
+    suspend fun parse(html: String, pageUrl: String): List<Torrent> =
+        withContext(Dispatchers.Default) {
+            Jsoup
+                .parse(html, pageUrl)
+                .select("table > tbody > tr:has(a.btn-success)")
+                .mapNotNull { parseTableRow(it) }
         }
-    }
-
-    private fun parseResponseHtml(html: String): List<Torrent> {
-        return Jsoup
-            .parse(html)
-            .select("table > tbody > tr:has(a.btn-success)")
-            .mapNotNull { parseTableRow(it) }
-    }
 
     private fun parseTableRow(tr: Element): Torrent? {
         val torrentName = tr.selectFirst("td.dn-title")?.text() ?: return null
@@ -50,8 +60,7 @@ class FileMood : SearchProvider {
         val uploadDate = "0m ago"
         // #result-main-center > div > div > table:nth-child(2) > tbody > tr:nth-child(1) > td.dn-btn > div > a
         val descriptionPageUrl = tr.selectFirst("td.dn-btn > div > a")
-            ?.attr("href")
-            ?.let { "${info.url}$it" }
+            ?.attr("abs:href")
             ?: return null
         val infoHash = descriptionPageUrl
             .removeSuffix(".html")
@@ -67,8 +76,41 @@ class FileMood : SearchProvider {
             peers = peers.toUIntOrNull() ?: 0U,
             uploadDate = uploadDate,
             category = null,
-            providerName = info.name,
+            providerName = providerName,
             descriptionPageUrl = descriptionPageUrl,
+        )
+    }
+}
+
+private object FileMoodDetailsPageParser {
+    private const val NAME =
+        "#content-main-center > div > div > div.well > table:nth-child(1) > tbody > tr:nth-child(1) > td > h1 > b"
+    private const val SIZE =
+        "#content-main-center > div > div > div.well > table:nth-child(3) > tbody > tr:nth-child(2) > td:nth-child(2) > p > b"
+    private const val LAST_CHECKED =
+        "#content-main-center > div > div > div.well > table:nth-child(3) > tbody > tr:nth-child(4) > td:nth-child(2) > p > b"
+    private const val INFO_HASH =
+        "#content-main-center > div > div > div.well > table:nth-child(3) > tbody > tr:nth-child(5) > td:nth-child(2) > p"
+
+    suspend fun parse(html: String): TorrentDetails? = withContext(Dispatchers.Default) {
+        val html = Jsoup.parse(html)
+
+        val name = html.selectFirst(NAME)?.ownText() ?: return@withContext null
+        val infoHash = html.selectFirst(INFO_HASH)?.ownText()?.lowercase()?.trim()
+            ?: return@withContext null
+        val magnetUri = TorrentUtils.createMagnetUri(infoHash)
+        val size = html.selectFirst(SIZE)?.ownText() ?: "0 KB"
+        val lastChecked = html.selectFirst(LAST_CHECKED)?.ownText()
+
+        TorrentDetails(
+            infoHash = infoHash,
+            name = name,
+            size = size,
+            seeders = 1U,
+            peers = 1U,
+            uploadDate = "0 min ago",
+            lastChecked = lastChecked,
+            magnetUri = magnetUri,
         )
     }
 }
