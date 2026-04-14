c92a9bb7e2eb52b44ca6416b00ff16283811f295
prajwalch <prajwal.chapagain58@gmail.com>
2026-04-14 22:06:33 +0545

    feat(XXXTracker): Add support for details page
    Signed-off-by: prajwalch <prajwal.chapagain58@gmail.com>

--- app/src/main/kotlin/com/prajwalch/torrentsearch/providers/XXXTracker.kt
+++ app/src/main/kotlin/com/prajwalch/torrentsearch/providers/XXXTracker.kt
@@ -2,6 +2,8 @@ package com.prajwalch.torrentsearch.providers
 
 import com.prajwalch.torrentsearch.domain.model.Category
 import com.prajwalch.torrentsearch.domain.model.Torrent
+import com.prajwalch.torrentsearch.domain.model.TorrentDetails
+import com.prajwalch.torrentsearch.network.HttpClient
 import com.prajwalch.torrentsearch.util.TorrentUtils
 
 import kotlinx.coroutines.Dispatchers
@@ -33,6 +35,11 @@ class XXXTracker : SearchProvider {
         }
 
         return torrents.orEmpty()
+    }
+
+    override suspend fun getDetails(detailsPageUrl: String): TorrentDetails? {
+        val responseHtml = HttpClient.get(detailsPageUrl)
+        return XXXTrackerDetailsPageParser.parse(html = responseHtml, pageUrl = detailsPageUrl)
     }
 
     private fun parseResponseHtml(html: String): List<Torrent>? {
@@ -78,4 +85,49 @@ class XXXTracker : SearchProvider {
             fileDownloadLink = fileDownloadLink,
         )
     }
+}
+
+private object XXXTrackerDetailsPageParser {
+    private const val NAME = "#content > h1"
+    private const val SIZE = "#details > tbody > tr:nth-last-child(2) > td:nth-child(2)"
+    private const val SEEDERS = "#details > tbody > tr:nth-last-child(7) > td:nth-child(2)"
+    private const val PEERS = "#details > tbody > tr:nth-last-child(6) > td:nth-child(2)"
+    private const val UPLOAD_DATE = "#details > tbody > tr:nth-last-child(3) > td:nth-child(2)"
+    private const val MAGNET_URI = """a[href^="magnet:?"]"""
+    private const val FILE_DOWNLOAD_LINK = "#download > a:nth-child(1)"
+    private const val POSTER_URL = "#details > tbody > tr:nth-child(1) > td:nth-child(2) > img"
+    private const val DESCRIPTION = "#details > tbody > tr:nth-child(1) > td:nth-child(2)"
+
+    suspend fun parse(html: String, pageUrl: String): TorrentDetails? =
+        withContext(Dispatchers.Default) {
+            val html = Jsoup.parse(html, pageUrl)
+
+            val name = html.selectFirst(NAME)?.text() ?: return@withContext null
+            val magnetUri = html.selectFirst(MAGNET_URI)?.attr("href") ?: return@withContext null
+            val infoHash = TorrentUtils.getInfoHashFromMagnetUri(magnetUri)
+            val size = html.selectFirst(SIZE)?.text()?.takeWhile { it != '(' }?.trim()
+            val seeders = html.selectFirst(SEEDERS)?.text()?.toUIntOrNull()
+            val peers = html.selectFirst(PEERS)?.text()?.toUIntOrNull()
+            val uploadDate = html.selectFirst(UPLOAD_DATE)?.text()
+            val fileDownloadLink = html.selectFirst(FILE_DOWNLOAD_LINK)?.attr("abs:href")
+            val posterUrl = html.selectFirst(POSTER_URL)?.attr("src")
+            val description = html.selectFirst(DESCRIPTION)
+                // Remove poster and two new lines after the poster from description.
+                ?.apply { select("> *:lt(3)").remove() }
+                ?.html()
+
+            TorrentDetails(
+                infoHash = infoHash,
+                name = name,
+                size = size,
+                seeders = seeders,
+                peers = peers,
+                uploadDate = uploadDate,
+                category = Category.Porn.name,
+                magnetUri = magnetUri,
+                fileDownloadLink = fileDownloadLink,
+                description = description,
+                posterUrl = posterUrl,
+            )
+        }
 }
