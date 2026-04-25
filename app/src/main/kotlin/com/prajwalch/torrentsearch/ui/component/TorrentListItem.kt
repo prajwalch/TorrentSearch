eaa6e75ed352c16ebf4ec02b7bed29f0f342ac8c
prajwalch <prajwal.chapagain58@gmail.com>
2026-04-25 21:53:20 +0545

    feat(TorrentListItem): Revamp UI
    - Use leading category icon instead of category badge
    - Move upload date from overline to trailing side
    - Remove provider badge and display provider name directly at overline
    - Use body medium typography with semi bold font weight for headline
    - Use body small for supporting content (size, seeders and peers)
    - Remove 'Seeders` and `Peers` label and instead use icon
    - Move NSFW bage to overline with provider name
    
    Signed-off-by: prajwalch <prajwal.chapagain58@gmail.com>

--- app/src/main/kotlin/com/prajwalch/torrentsearch/ui/component/TorrentListItem.kt
+++ app/src/main/kotlin/com/prajwalch/torrentsearch/ui/component/TorrentListItem.kt
@@ -1,23 +1,32 @@
 package com.prajwalch.torrentsearch.ui.component
 
+import androidx.annotation.DrawableRes
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Icon
 import androidx.compose.material3.ListItem
+import androidx.compose.material3.LocalContentColor
+import androidx.compose.material3.LocalTextStyle
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 import com.prajwalch.torrentsearch.R
 import com.prajwalch.torrentsearch.domain.model.Category
 import com.prajwalch.torrentsearch.ui.iconResId
+import com.prajwalch.torrentsearch.ui.theme.TorrentSearchTheme
 import com.prajwalch.torrentsearch.ui.theme.spaces
 
 @Composable
@@ -37,37 +46,39 @@ fun TorrentListItem(
 
     ListItem(
         modifier = modifier.alpha(contentAlpha),
-        overlineContent = { Text(text = uploadDate) },
         leadingContent = {
             Icon(
                 painter = painterResource(category.iconResId()),
                 contentDescription = null,
             )
         },
+        overlineContent = {
+            Row(verticalAlignment = Alignment.CenterVertically) {
+                Text(providerName)
+
+                if (isNSFW) {
+                    BulletPoint()
+                    NSFWBadge()
+                }
+            }
+        },
         headlineContent = {
             Text(
                 text = name,
+                fontWeight = FontWeight.SemiBold,
                 overflow = TextOverflow.Ellipsis,
                 maxLines = 3,
+                style = MaterialTheme.typography.bodyMedium,
             )
         },
+        trailingContent = { Text(uploadDate) },
         supportingContent = {
-            Column(
-                verticalArrangement = Arrangement.spacedBy(
-                    space = MaterialTheme.spaces.extraSmall,
-                    alignment = Alignment.CenterVertically,
+            CompositionLocalProvider(
+                LocalTextStyle provides MaterialTheme.typography.bodySmall.copy(
+                    fontFeatureSettings = "tnum",
                 ),
             ) {
-                TorrentMetadata(
-                    size = size,
-                    seeders = seeders,
-                    peers = peers,
-                )
-                BadgeRow {
-                    category?.let { CategoryBadge(category = it) }
-                    SearchProviderBadge(name = providerName)
-                    if (isNSFW) NSFWBadge()
-                }
+                TorrentMetadata(size = size, seeders = seeders, peers = peers)
             }
         },
     )
@@ -82,29 +93,86 @@ private fun TorrentMetadata(
 ) {
     Row(
         modifier = modifier,
-        horizontalArrangement = Arrangement.spacedBy(
-            space = MaterialTheme.spaces.extraSmall,
-        ),
+        horizontalArrangement = Arrangement.spacedBy(MaterialTheme.spaces.extraSmall),
         verticalAlignment = Alignment.CenterVertically,
     ) {
-        TorrentMetadataText(text = size)
+        TorrentMetadataItem(text = size)
         BulletPoint()
-        TorrentMetadataText(text = stringResource(R.string.torrent_list_seeders_format, seeders))
+        TorrentMetadataItem(
+            icon = R.drawable.ic_upload,
+            text = seeders.toString(),
+            color = MaterialTheme.colorScheme.secondary,
+            textStyle = LocalTextStyle.current.copy(fontWeight = FontWeight.SemiBold),
+        )
         BulletPoint()
-        TorrentMetadataText(text = stringResource(R.string.torrent_list_peers_format, peers))
+        TorrentMetadataItem(
+            icon = R.drawable.ic_download,
+            text = peers.toString(),
+            color = MaterialTheme.colorScheme.tertiary
+        )
     }
 }
 
 @Composable
-private fun TorrentMetadataText(text: String, modifier: Modifier = Modifier) {
-    Text(
+private fun TorrentMetadataItem(
+    text: String,
+    modifier: Modifier = Modifier,
+    @DrawableRes icon: Int? = null,
+    color: Color = LocalContentColor.current,
+    textStyle: TextStyle = LocalTextStyle.current,
+) {
+    Row(
         modifier = modifier,
-        text = text,
-        color = MaterialTheme.colorScheme.secondary,
-    )
+        horizontalArrangement = Arrangement.spacedBy(MaterialTheme.spaces.extraSmall),
+        verticalAlignment = Alignment.CenterVertically,
+    ) {
+        icon?.let {
+            Icon(
+                modifier = Modifier.size(16.dp),
+                painter = painterResource(it),
+                contentDescription = null,
+                tint = color,
+            )
+        }
+        Text(text = text, color = color, style = textStyle)
+    }
 }
 
 @Composable
 private fun BulletPoint(modifier: Modifier = Modifier) {
     Text(modifier = modifier, text = "\u2022")
+}
+
+@Preview
+@Composable
+private fun TorrentListItemDarkPreview() {
+    TorrentSearchTheme(darkTheme = true) {
+        TorrentListItem(
+            name = "Sinners.2025.1080p [Yts] [H.265] something",
+            size = "2.1 GB",
+            seeders = 200u,
+            peers = 5000u,
+            uploadDate = "13 Apr 2025",
+            category = Category.Movies,
+            providerName = "UIndex",
+            isNSFW = false,
+        )
+    }
+}
+
+@Preview
+@Composable
+private fun TorrentListItemLightPreview() {
+    TorrentSearchTheme {
+        TorrentListItem(
+            name = "Sinners.2025.1080p [Yts] [H.265] something",
+            size = "2.1 GB",
+            seeders = 200u,
+            peers = 5000u,
+            uploadDate = "13 Apr 2025",
+            category = Category.Movies,
+            providerName = "UIndex",
+            isNSFW = false,
+        )
+    }
 }
