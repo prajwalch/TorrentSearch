f1279b2f9d5a8b3fe70fe8146031b7104c735dd8
prajwalch <prajwal.chapagain58@gmail.com>
2026-04-28 21:45:12 +0545

    feat(TorrentDescription): Properly configure markdown renderer
    - Use our globally configured coil image loader
    - Make text selectable
    - Use default text selection colors
    - Use `surfaceContainerHigh` color scheme for the syntax
    - Configure it to linkify only web urls
    
    Signed-off-by: prajwalch <prajwal.chapagain58@gmail.com>

--- app/src/main/kotlin/com/prajwalch/torrentsearch/ui/torrentdetails/component/TorrentDescription.kt
+++ app/src/main/kotlin/com/prajwalch/torrentsearch/ui/torrentdetails/component/TorrentDescription.kt
@@ -1,35 +1,40 @@
 package com.prajwalch.torrentsearch.ui.torrentdetails.component
 
+import android.text.util.Linkify
+import androidx.compose.foundation.text.selection.LocalTextSelectionColors
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontStyle
 
+import coil3.SingletonImageLoader
 import com.prajwalch.torrentsearch.R
-
 import dev.jeziellago.compose.markdowntext.MarkdownText
 
 @Composable
 fun TorrentDescription(description: String?, modifier: Modifier = Modifier) {
+    val context = LocalContext.current
+
     DetailsSection(
         modifier = modifier,
         title = { Text(stringResource(R.string.torrent_details_title_description)) },
     ) {
         if (description != null) {
-            // TODO: Normalize HTML contained description.
             MarkdownText(
                 markdown = description,
                 linkColor = MaterialTheme.colorScheme.primary,
+                isTextSelectable = true,
+                textSelectionColors = LocalTextSelectionColors.current,
+                imageLoader = SingletonImageLoader.get(context),
+                linkifyMask = Linkify.WEB_URLS,
                 enableSoftBreakAddsNewLine = true,
-                syntaxHighlightColor = MaterialTheme.colorScheme.tertiaryContainer,
-                syntaxHighlightTextColor = MaterialTheme.colorScheme.onTertiaryContainer,
+                syntaxHighlightColor = MaterialTheme.colorScheme.surfaceContainerHigh,
+                syntaxHighlightTextColor = MaterialTheme.colorScheme.onSurface,
                 enableUnderlineForLink = true,
             )
-//            SelectionContainer {
-//                Text(text = AnnotatedString.fromHtml(description))
-//            }
         } else {
             Text(
                 text = stringResource(R.string.torrent_details_message_not_available),
