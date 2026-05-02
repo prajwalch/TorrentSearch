45c5f50a3424f6fd317a1a80f21c71c19a74f0bc
prajwalch <prajwal.chapagain58@gmail.com>
2026-05-02 18:24:23 +0545

    refactor(Dmhy): Polish results page parser implementation
    Signed-off-by: prajwalch <prajwal.chapagain58@gmail.com>

--- app/src/main/kotlin/com/prajwalch/torrentsearch/providers/Dmhy.kt
+++ app/src/main/kotlin/com/prajwalch/torrentsearch/providers/Dmhy.kt
@@ -20,6 +20,8 @@ class Dmhy : SearchProvider {
     override val safetyStatus = SearchProviderSafetyStatus.Safe
     override val enabledByDefault = false
 
+    private val resultsPageParser = DmhyResultsPageParser(providerName = name)
+
     override suspend fun search(query: String, context: SearchContext): List<Torrent> {
         val requestUrl = buildString {
             append(url)
@@ -29,82 +31,70 @@ class Dmhy : SearchProvider {
         }
         val responseHtml = context.httpClient.get(url = requestUrl)
 
-        return withContext(Dispatchers.Default) {
-            parseResponseHtml(html = responseHtml)
+        return resultsPageParser.parse(html = responseHtml, pageUrl = requestUrl)
+    }
+}
+
+private class DmhyResultsPageParser(
+    private val providerName: String,
+) {
+    suspend fun parse(html: String, pageUrl: String): List<Torrent> =
+        withContext(Dispatchers.Default) {
+            Jsoup
+                .parse(html, pageUrl)
+                .select(LIST_ITEM)
+                .mapNotNull(::parseListItem)
         }
-    }
 
-    private fun parseResponseHtml(html: String): List<Torrent> {
-        return Jsoup
-            .parse(html)
-            .select("table tbody tr:has(a[href^=\"magnet:?\"])")
-            .mapNotNull(::parseTableRow)
-    }
-
-    private fun parseTableRow(tr: Element): Torrent? {
-        val uploadDate = tr
-            .selectFirst("td:nth-child(1) span")
-            ?.ownText()
+    private fun parseListItem(listItem: Element): Torrent? {
+        val torrentName = listItem.selectFirst(TORRENT_NAME)?.ownText() ?: return null
+        val magnetUri = listItem.selectFirst(MAGNET_URI)?.attr("href") ?: return null
+        val size = listItem.selectFirst(SIZE)?.ownText()?.let(FileSizeUtils::normalizeSize)
+        val seeders = listItem.selectFirst(SEEDERS)?.text()?.toUIntOrNull()
+        val peers = listItem.selectFirst(PEERS)?.text()?.toUIntOrNull()
+        val uploadDate = listItem.selectFirst(UPLOAD_DATE)?.ownText()
             ?.split(' ')
             ?.firstOrNull()
             ?.replace("/", "-")
             ?.let(DateUtils::formatYearMonthDay)
-            ?: return null
-        val category = tr
-            .selectFirst("td:nth-child(2) a")
-            ?.attr("href")
-            ?.removePrefix("/topics/list/sort_id/")
+        val category = listItem.selectFirst(CATEGORY)?.className()
+            ?.removePrefix("sort-")
             ?.let(::getCategoryFromId)
-            ?: return null
-
-        val titleHref = tr
-            .selectFirst("td.title a")
-            ?: return null
-        val torrentName = titleHref.ownText()
-        val descriptionPageUrl = titleHref.attr("href").let { "$url$it" }
-
-        val magnetUri = tr
-            .selectFirst("""a[href^="magnet:"]""")
-            ?.attr("href")
-            ?: return null
-        val infoHash = TorrentUtils.getInfoHashFromMagnetUri(magnetUri)
-        val size = tr
-            .selectFirst("td:nth-child(5)")
-            ?.ownText()
-            ?.let(FileSizeUtils::normalizeSize)
-            ?: return null
-        val seeders = tr
-            .selectFirst("td:nth-child(6)")
-            ?.ownText()
-            ?.toUIntOrNull()
-            ?: 0u
-        val peers = tr
-            .selectFirst("td:nth-child(7)")
-            ?.ownText()
-            ?.toUIntOrNull()
-            ?: 0u
+        val detailsPageUrl = listItem.selectFirst(DETAILS_PAGE_URL)?.attr("abs:href")
 
         return Torrent(
-            infoHash = infoHash,
+            infoHash = TorrentUtils.getInfoHashFromMagnetUri(magnetUri),
             name = torrentName,
-            size = size,
-            seeders = seeders,
-            peers = peers,
-            uploadDate = uploadDate,
+            size = size ?: "0 KB",
+            seeders = seeders ?: 1U,
+            peers = peers ?: 1U,
+            uploadDate = uploadDate ?: "0 min ago",
             category = category,
-            providerName = name,
-            descriptionPageUrl = descriptionPageUrl,
+            providerName = providerName,
             magnetUri = magnetUri,
+            descriptionPageUrl = detailsPageUrl ?: "",
         )
     }
 
-    private fun getCategoryFromId(id: String) = when (id) {
-        "2", "7", "31" -> Category.Anime
-        "3" -> Category.Books
-        "41", "42" -> Category.Series
-        "4", "43", "44", "15" -> Category.Music
-        "6" -> Category.Series
-        "9", "17", "18", "19", "20", "21" -> Category.Games
-        else -> Category.Other
+    private companion object {
+        private const val LIST_ITEM = "table#topic_list > tbody > tr"
+        private const val TORRENT_NAME = "td.title > a"
+        private const val SIZE = "td:nth-child(5)"
+        private const val SEEDERS = "td:nth-child(6)"
+        private const val PEERS = "td:nth-child(7)"
+        private const val UPLOAD_DATE = "td:nth-child(1)"
+        private const val CATEGORY = "td:nth-child(2) > a"
+        private const val MAGNET_URI = "td:nth-child(4) > a:nth-child(1)"
+        private const val DETAILS_PAGE_URL = TORRENT_NAME
     }
+}
+
+private fun getCategoryFromId(id: String) = when (id) {
+    "2", "7", "31" -> Category.Anime
+    "3" -> Category.Books
+    "41", "42" -> Category.Series
+    "4", "43", "44", "15" -> Category.Music
+    "6" -> Category.Series
+    "9", "17", "18", "19", "20", "21" -> Category.Games
+    else -> Category.Other
 }
