326a3d5f6862129cbff9a61fc0021c31013e3467
prajwalch <prajwal.chapagain58@gmail.com>
2026-05-22 19:43:49 +0545

    feat(SubsPlease): Implement torrent details provider
    Signed-off-by: prajwalch <prajwal.chapagain58@gmail.com>

--- app/src/main/kotlin/com/prajwalch/torrentsearch/providers/SubsPlease.kt
+++ app/src/main/kotlin/com/prajwalch/torrentsearch/providers/SubsPlease.kt
@@ -5,8 +5,10 @@ import androidx.core.net.toUri
 import com.prajwalch.torrentsearch.domain.model.Category
 import com.prajwalch.torrentsearch.domain.model.MagnetUri
 import com.prajwalch.torrentsearch.domain.model.Torrent
+import com.prajwalch.torrentsearch.domain.model.TorrentDetails
 import com.prajwalch.torrentsearch.extension.asObject
 import com.prajwalch.torrentsearch.extension.getArray
+import com.prajwalch.torrentsearch.extension.getObject
 import com.prajwalch.torrentsearch.extension.getString
 import com.prajwalch.torrentsearch.network.HttpClient
 import com.prajwalch.torrentsearch.util.FileSizeUtils
@@ -18,9 +20,10 @@ import kotlinx.coroutines.withContext
 import kotlinx.serialization.json.JsonElement
 import kotlinx.serialization.json.JsonObject
 
+import org.jsoup.Jsoup
 import java.time.Instant
 
-class SubsPlease : SearchProvider, LatestTorrentsProvider {
+class SubsPlease : SearchProvider, LatestTorrentsProvider, TorrentDetailsProvider {
     override val id = "subsplease"
     override val name = "SubsPlease"
     override val url = "https://subsplease.org"
@@ -50,6 +53,11 @@ class SubsPlease : SearchProvider, LatestTorrentsProvider {
 
         return resultsJsonParser.parse(responseJson)
     }
+
+    override suspend fun getDetails(detailsPageUrl: String): TorrentDetails? {
+        val responseHtml = HttpClient.get(detailsPageUrl)
+        return SubsPleaseDetailsPageParser.parse(html = responseHtml, pageUrl = detailsPageUrl)
+    }
 }
 
 private class SubsPleaseResultsJsonParser(
@@ -71,6 +79,7 @@ private class SubsPleaseResultsJsonParser(
     private fun parseAnimeObject(showName: String, animeObject: JsonObject): List<Torrent>? {
         val uploadDate = animeObject.getString("release_date")?.let(TorrentDateParser::parseRFC1123)
         val detailsPageUrl = animeObject.getString("page")?.let { "$providerUrl/$it" }
+        val episodeNumber = animeObject.getString("episode")!!
 
         return animeObject.getArray("downloads")?.mapNotNull {
             parseDownloadObject(
@@ -78,6 +87,7 @@ private class SubsPleaseResultsJsonParser(
                 torrentName = showName,
                 uploadDate = uploadDate,
                 detailsPageUrl = detailsPageUrl,
+                episodeNumber = episodeNumber,
             )
         }
     }
@@ -87,12 +97,15 @@ private class SubsPleaseResultsJsonParser(
         torrentName: String,
         uploadDate: Instant?,
         detailsPageUrl: String?,
+        episodeNumber: String,
     ): Torrent? {
         val magnetUri = downloadObject.getString("magnet") ?: return null
-        val size = parseSizeFromMagnetUri(magnetUri = magnetUri)
+        val size = SubsPleaseUtils.parseSizeFromMagnetUri(magnetUri = magnetUri)
 
-        val resolution = downloadObject.getString("res")?.let { "${it}p" }
-        val finalTorrentName = if (resolution == null) torrentName else "$torrentName [$resolution]"
+        val resolution = downloadObject.getString("res")!!
+        val finalTorrentName = "$torrentName [${resolution}p]"
+
+        val detailsPageUrl = detailsPageUrl?.let { "$it?ep=$episodeNumber&res=$resolution" }
 
         return Torrent(
             infoHash = TorrentUtils.getInfoHashFromMagnetUri(magnetUri),
@@ -107,12 +120,86 @@ private class SubsPleaseResultsJsonParser(
             providerName = providerName,
         )
     }
+}
 
-    private fun parseSizeFromMagnetUri(magnetUri: MagnetUri): String? {
+private object SubsPleaseDetailsPageParser {
+    suspend fun parse(html: String, pageUrl: String): TorrentDetails? =
+        withContext(Dispatchers.Default) {
+            val html = Jsoup.parse(html, pageUrl)
+
+            val showId = html.selectFirst("table#show-release-table")
+                ?.attr("sid")
+                ?: return@withContext null
+            val showName = html.selectFirst("h1.entry-title")?.ownText() ?: return@withContext null
+            val posterUrl = html.selectFirst("img.img-responsive.img-center")?.attr("abs:src")
+            val description = html.selectFirst("div.series-syn > p")?.ownText()
+
+            val (episodeNumber, torrentResolution) = pageUrl.takeLastWhile { it != '?' }
+                .split("&", limit = 2)
+                .let { (episodeParam, resolutionParam) ->
+                    Pair(
+                        episodeParam.removePrefix("ep="),
+                        resolutionParam.removePrefix("res="),
+                    )
+                }
+
+            val (episodeObject, downloadObject) = getEpisodeAndDownloadObject(
+                showId = showId,
+                episodeNumber = episodeNumber,
+                torrentResolution = torrentResolution
+            ) ?: return@withContext null
+
+            val torrentName = "$showName - $episodeNumber [${torrentResolution}p]"
+            val magnetUri = downloadObject.getString("magnet") ?: return@withContext null
+            val infoHash = TorrentUtils.getInfoHashFromMagnetUri(magnetUri)
+            val size = SubsPleaseUtils.parseSizeFromMagnetUri(magnetUri)
+            val uploadDate = episodeObject.getString("time")?.let {
+                TorrentDateParser.parse(date = it, format = "MM/dd/yy")
+            }
+            val fileDownloadLink = downloadObject.getString("torrent")
+
+            TorrentDetails(
+                infoHash = infoHash,
+                name = torrentName,
+                size = size,
+                uploadDate = uploadDate,
+                category = Category.Anime,
+                magnetUri = magnetUri,
+                fileDownloadLink = fileDownloadLink,
+                description = description,
+                posterUrl = posterUrl,
+            )
+        }
+
+    suspend fun getEpisodeAndDownloadObject(
+        showId: String,
+        episodeNumber: String,
+        torrentResolution: String,
+    ): Pair<JsonObject, JsonObject>? = withContext(Dispatchers.IO) {
+        val requestUrl = "https://subsplease.org/api/?f=show&tz=$&sid=$showId"
+        val responseJson = HttpClient.getJson(requestUrl) ?: return@withContext null
+
+        val episodeObject = responseJson.asObject()
+            .getObject("episode")
+            ?.values
+            ?.map { it.asObject() }
+            ?.find { it.getString("episode") == episodeNumber }
+            ?: return@withContext null
+
+        val downloadObject = episodeObject.getArray("downloads")
+            ?.map { it.asObject() }
+            ?.find { it.getString("res") == torrentResolution }
+            ?: return@withContext null
+
+        Pair(episodeObject, downloadObject)
+    }
+}
+
+private object SubsPleaseUtils {
+    fun parseSizeFromMagnetUri(magnetUri: MagnetUri): String? {
         val magnetUri = magnetUri.toUri()
         // android.net.Uri.getQueryParamater() is not supported for magnet URI.
-        return magnetUri
-            .query
+        return magnetUri.query
             ?.split('&')
             ?.firstOrNull { it.startsWith("xl=") }
             ?.removePrefix("xl=")
