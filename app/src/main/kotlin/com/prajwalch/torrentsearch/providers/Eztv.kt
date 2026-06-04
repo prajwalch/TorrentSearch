4007b7b8f90f88843793de5d4f3b7113c9ae5ad0
prajwalch <prajwal.chapagain58@gmail.com>
2026-06-04 14:57:34 +0545

    refactor(Eztv): Polish results page parsing implementation
    Signed-off-by: prajwalch <prajwal.chapagain58@gmail.com>

--- app/src/main/kotlin/com/prajwalch/torrentsearch/providers/Eztv.kt
+++ app/src/main/kotlin/com/prajwalch/torrentsearch/providers/Eztv.kt
@@ -20,6 +20,8 @@ class Eztv : SearchProvider {
     override val isCloudflareProtected = true
     override val enabledByDefault = true
 
+    private val resultsPageParser = EztvResultsPageParser(name)
+
     override suspend fun search(query: String, context: SearchContext): List<Torrent> {
         val requestUrl = "$url/search/$query"
         // Without setting that cookie, it returns results without magnet links.
@@ -28,100 +30,48 @@ class Eztv : SearchProvider {
             headers = mapOf("Cookie" to "layout=def_wlinks"),
         )
 
-        return withContext(Dispatchers.Default) {
-            parseHtml(html = responseHtml)
-        }
+        return resultsPageParser.parse(html = responseHtml, pageUrl = requestUrl)
     }
+}
 
-    /** Parses the HTML and returns all the extracted [Torrent]s. */
-    private fun parseHtml(html: String): List<Torrent> {
-        // There are exactly 6 tables in the entire document, only the last one
-        // will contain the search results.
-        val resultsTableBody = Jsoup
-            .parse(html)
-            .select("table")
-            .last()
-            ?.selectFirst("tbody")
-            ?: return emptyList()
-
-        val tableRows = resultsTableBody.children()
-        // The first one is unimportant header while the second is the actual
-        // table header. Note that, it doesn't use the <thead> instead uses the
-        // <tr> itself.
-        //
-        // To make sure we select the correct table, check the header has
-        // expected data <td> or not.
-        val headerRow = tableRows.getOrNull(1) ?: return emptyList()
-        if (!validateHeader(headerRow)) {
-            return emptyList()
+private class EztvResultsPageParser(private val providerName: String) {
+    suspend fun parse(html: String, pageUrl: String): List<Torrent> =
+        withContext(Dispatchers.Default) {
+            Jsoup.parse(html, pageUrl)
+                .select(LIST_ITEM)
+                .drop(2)
+                .mapNotNull(::parseListItem)
         }
 
-        return tableRows
-            .subList(fromIndex = 2, toIndex = tableRows.size)
-            .mapNotNull { tr -> parseTableRow(tr = tr) }
-    }
-
-    /**
-     * Returns `true` if the header has expected elements and data.
-     *
-     * Header layout:
-     *
-     *    Show | Episode Name | Dload | Size | Released | Seeds
-     */
-    private fun validateHeader(headerTr: Element): Boolean {
-        // Checking only the first two would be sufficient? I guess?
-        val allTd = headerTr.select("td")
-
-        if (allTd.size != 6) {
-            return false
-        }
-
-        // "Show"
-        val firstTd = allTd.first() ?: return false
-        // "Episode Name"
-        val secondTd = allTd.getOrNull(1) ?: return false
-
-        return (firstTd.ownText() == "Show") && (secondTd.ownText() == "Episode Name")
-    }
-
-    /** Parses the table row and returns the [Torrent] if parse succeed. */
-    private fun parseTableRow(tr: Element): Torrent? {
-        val epInfoAnchorElement = tr.selectFirst("a.epinfo") ?: return null
-        val torrentName = epInfoAnchorElement.ownText()
-
-        val descriptionPagePath = epInfoAnchorElement.attr("href")
-        val descriptionPageUrl = "$url$descriptionPagePath"
-
-        val magnetUri = tr.selectFirst("a.magnet")?.attr("href") ?: return null
-        val infoHash = TorrentUtils.getInfoHashFromMagnetUri(magnetUri)
-        val size = tr.selectFirst("td:nth-child(4)")?.ownText() ?: return null
-        // TODO: The date format used the results page is 'time ago'
-        //       (e.g. '7h 8m', '1 week', '1 mo'). The format we want
-        //       is present in the details page. Let's extract it in future.
-//        val uploadDate = tr.selectFirst("td:nth-child(5)")?.ownText() ?: return null
-
-        // Some torrents will not have any seeds (no idea why), in that case
-        // it will contain '-' text node, and in other case it will contain a
-        // <font> with number of seeds as its text node.
-        //
-        // Additional note: It uses ',' separator (e.g. 1,000).
-        val seedsTd = tr.selectFirst("td:nth-child(6)") ?: return null
-        val seeders = seedsTd.selectFirst("font")?.ownText()?.filter { it != ',' } ?: "0"
-        // TODO: Peers is not present in the results page, we have to grab it
-        //       from the details page. Let's do that in future.
-        val peers = 0u
+    private fun parseListItem(listItem: Element): Torrent? {
+        val torrentName = listItem.selectFirst(TORRENT_NAME)?.ownText() ?: return null
+        val magnetUri = listItem.selectFirst(MAGNET_URI)?.attr("href") ?: return null
+        val size = listItem.selectFirst(SIZE)?.ownText()
+        val seeders = listItem.selectFirst(SEEDERS)?.ownText()?.toUIntOrNull()
+        val fileDownloadLink = listItem.selectFirst(FILE_DOWNLOAD_LINK)?.attr("abs:href")
+        val detailsPageUrl = listItem.selectFirst(DETAILS_PAGE_URL)?.attr("abs:href")
 
         return Torrent(
-            infoHash = infoHash,
+            infoHash = TorrentUtils.getInfoHashFromMagnetUri(magnetUri),
             name = torrentName,
-            size = size,
-            seeders = seeders.toUIntOrNull() ?: 0u,
-            peers = peers,
-            providerName = name,
-//            uploadDate = uploadDate,
+            size = size ?: "0 KB",
+            seeders = seeders ?: 0U,
+            peers = 0u,
             category = Category.Series,
-            descriptionPageUrl = descriptionPageUrl,
+            providerName = providerName,
             magnetUri = magnetUri,
+            fileDownloadLink = fileDownloadLink,
+            descriptionPageUrl = detailsPageUrl ?: "",
         )
+    }
+
+    private companion object {
+        private const val LIST_ITEM = "table:last-of-type > tbody > tr"
+        private const val TORRENT_NAME = "td:nth-child(2) > a.epinfo"
+        private const val SIZE = "td:nth-child(4)"
+        private const val SEEDERS = "td:nth-child(6)"
+        private const val MAGNET_URI = "td:nth-child(3) > a.magnet"
+        private const val FILE_DOWNLOAD_LINK = "td:nth-child(3) > a:nth-child(2)"
+        private const val DETAILS_PAGE_URL = TORRENT_NAME
     }
 }
