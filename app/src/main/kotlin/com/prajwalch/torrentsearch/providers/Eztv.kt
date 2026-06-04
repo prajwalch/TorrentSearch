58cbab3072a5d70dbf99df33086c9c71c7f8b81d
prajwalch <prajwal.chapagain58@gmail.com>
2026-06-04 18:18:37 +0545

    feat(Eztv): Implement torrent details provider
    Signed-off-by: prajwalch <prajwal.chapagain58@gmail.com>

--- app/src/main/kotlin/com/prajwalch/torrentsearch/providers/Eztv.kt
+++ app/src/main/kotlin/com/prajwalch/torrentsearch/providers/Eztv.kt
@@ -2,6 +2,9 @@ package com.prajwalch.torrentsearch.providers
 
 import com.prajwalch.torrentsearch.domain.model.Category
 import com.prajwalch.torrentsearch.domain.model.Torrent
+import com.prajwalch.torrentsearch.domain.model.TorrentDetails
+import com.prajwalch.torrentsearch.network.HttpClient
+import com.prajwalch.torrentsearch.util.TorrentDateParser
 import com.prajwalch.torrentsearch.util.TorrentUtils
 
 import kotlinx.coroutines.Dispatchers
@@ -9,8 +12,9 @@ import kotlinx.coroutines.withContext
 
 import org.jsoup.Jsoup
 import org.jsoup.nodes.Element
+import org.jsoup.nodes.TextNode
 
-class Eztv : SearchProvider {
+class Eztv : SearchProvider, TorrentDetailsProvider {
     override val id = "eztvx"
     override val name = "Eztv"
     override val url = "https://eztvx.to"
@@ -31,6 +35,11 @@ class Eztv : SearchProvider {
         )
 
         return resultsPageParser.parse(html = responseHtml, pageUrl = requestUrl)
+    }
+
+    override suspend fun getDetails(detailsPageUrl: String): TorrentDetails? {
+        val responseHtml = HttpClient.get(detailsPageUrl)
+        return EztvDetailsPageParser.parse(html = responseHtml, pageUrl = detailsPageUrl)
     }
 }
 
@@ -74,4 +83,63 @@ private class EztvResultsPageParser(private val providerName: String) {
         private const val FILE_DOWNLOAD_LINK = "td:nth-child(3) > a:nth-child(2)"
         private const val DETAILS_PAGE_URL = TORRENT_NAME
     }
+}
+
+private object EztvDetailsPageParser {
+    private const val TORRENT_NAME =
+        "#header_holder > table > tbody > tr:nth-child(1) > td > h1 > span"
+    private const val TORRENT_INFO_CONTAINER =
+        "#header_holder > table > tbody > tr:nth-child(2) > td > table.episode_columns_holder > tbody > tr:nth-child(1) > td:nth-child(3) > table > tbody > tr:nth-child(2) > td > table > tbody > tr:nth-child(1) > td"
+    private const val SEEDERS =
+        "#header_holder > table > tbody > tr:nth-child(2) > td > table.episode_columns_holder > tbody > tr:nth-child(1) > td.episode_middle_column > table:nth-child(2) > tbody > tr:nth-child(2) > td > div > table > tbody > tr > td:nth-child(2) > span.stat_red"
+    private const val PEERS =
+        "#header_holder > table > tbody > tr:nth-child(2) > td > table.episode_columns_holder > tbody > tr:nth-child(1) > td.episode_middle_column > table:nth-child(2) > tbody > tr:nth-child(2) > td > div > table > tbody > tr > td:nth-child(2) > span.stat_green"
+    private const val MAGNET_URI = """a[title="Magnet Link"]"""
+    private const val FILE_DOWNLOAD_LINK = """a[title="Download Torrent"]"""
+    private const val DESCRIPTION = "div.desc_big"
+    private const val POSTER_URL =
+        "#header_holder > table > tbody > tr:nth-child(2) > td > table.episode_columns_holder > tbody > tr:nth-child(1) > td.episode_left_column > table > tbody > tr:nth-child(2) > td > a:nth-child(1) > img"
+
+    suspend fun parse(html: String, pageUrl: String): TorrentDetails? =
+        withContext(Dispatchers.Default) {
+            val html = Jsoup.parse(html, pageUrl)
+
+            val torrentName = html.selectFirst(TORRENT_NAME)?.ownText() ?: return@withContext null
+            val magnetUri = html.selectFirst(MAGNET_URI)?.attr("href") ?: return@withContext null
+            val seeders = html.selectFirst(SEEDERS)?.ownText()?.toUIntOrNull()
+            val peers = html.selectFirst(PEERS)?.ownText()?.toUIntOrNull()
+
+            val torrentInfoContainer = html.selectFirst(TORRENT_INFO_CONTAINER)
+            val size = torrentInfoContainer
+                ?.find { it.ownText() == "Filesize:" }
+                ?.nextSibling()
+                ?.takeIf { it is TextNode }
+                ?.nodeValue()
+                ?.trim()
+            val uploadDate = torrentInfoContainer
+                ?.find { it.ownText() == "Released:" }
+                ?.nextSibling()
+                ?.takeIf { it is TextNode }
+                ?.nodeValue()
+                ?.trim()
+                ?.replace(Regex("(\\d+)(st|nd|rd|th)"), "$1")
+                ?.let { TorrentDateParser.parse(date = it, format = "d MMM yyyy") }
+            val fileDownloadLink = html.selectFirst(FILE_DOWNLOAD_LINK)?.attr("abs:href")
+            val description = html.selectFirst(DESCRIPTION)?.html()
+            val posterUrl = html.selectFirst(POSTER_URL)?.attr("abs:src")
+
+            TorrentDetails(
+                infoHash = TorrentUtils.getInfoHashFromMagnetUri(magnetUri),
+                name = torrentName,
+                size = size,
+                seeders = seeders,
+                peers = peers,
+                uploadDate = uploadDate,
+                category = Category.Series,
+                magnetUri = magnetUri,
+                fileDownloadLink = fileDownloadLink,
+                description = description,
+                posterUrl = posterUrl,
+            )
+        }
 }
