54365e4df2c4428cbbdd3b0479e82f5f145ed9f4
prajwalch <prajwal.chapagain58@gmail.com>
2026-06-07 11:51:24 +0545

    refactor: Improve Cloudflare WebView implementation
    Signed-off-by: prajwalch <prajwal.chapagain58@gmail.com>

--- app/src/main/kotlin/com/prajwalch/torrentsearch/ui/settings/searchproviders/cloudflare/component/CloudflareWebView.kt
+++ app/src/main/kotlin/com/prajwalch/torrentsearch/ui/settings/searchproviders/cloudflare/component/CloudflareWebView.kt
@@ -2,13 +2,8 @@ package com.prajwalch.torrentsearch.ui.settings.searchproviders.cloudflare.compo
 
 import android.annotation.SuppressLint
 import android.content.Context
-import android.util.Log
 import android.view.ViewGroup
-import android.webkit.WebResourceError
-import android.webkit.WebResourceRequest
-import android.webkit.WebResourceResponse
 import android.webkit.WebView
-import android.webkit.WebViewClient
 
 import androidx.compose.animation.animateContentSize
 import androidx.compose.animation.core.Spring
@@ -49,11 +44,6 @@ fun BoxedCloudflareWebView(
             .animateContentSize(spring(dampingRatio = Spring.DampingRatioLowBouncy))
             .height(height)
             .clip(shape = MaterialTheme.shapes.medium)
-//            .border(
-//                width = Dp.Hairline,
-//                color = MaterialTheme.colorScheme.outline,
-//                shape = MaterialTheme.shapes.medium,
-//            )
             .clipToBounds()
             .alpha(boxAlpha),
     ) {
@@ -74,7 +64,7 @@ fun CloudflareWebView(
     AndroidView(
         modifier = modifier,
         factory = { context ->
-            createWebView(
+            createCloudflareWebView(
                 context = context,
                 onChallengeSolved = onChallengeSolved,
             ).apply { loadUrl(url) }
@@ -87,7 +77,7 @@ fun CloudflareWebView(
 }
 
 @SuppressLint("SetJavaScriptEnabled")
-private fun createWebView(
+fun createCloudflareWebView(
     context: Context,
     onChallengeSolved: () -> Unit,
 ): WebView = WebView(context).apply {
@@ -106,68 +96,4 @@ private fun createWebView(
 //    }
 
     webViewClient = CloudflareWebViewClient(onChallengeSolved)
-}
-
-private class CloudflareWebViewClient(
-    private val onChallengeSolved: () -> Unit,
-) : WebViewClient() {
-    override fun onPageFinished(view: WebView?, url: String?) {
-        super.onPageFinished(view, url)
-
-        if (url == null) return
-        Log.i(TAG, "Finished loading $url")
-        Log.i(TAG, "Checking Cloudflare clearance cookie for $url")
-
-        val cookie = HttpClient.getCookie(url)
-        if (cookie != null && cookie.contains("cf_clearance")) {
-            Log.i(TAG, "Found the cookie")
-            onChallengeSolved()
-        } else {
-            Log.w(TAG, "Cookie not found")
-        }
-    }
-
-    override fun onReceivedError(
-        view: WebView?,
-        request: WebResourceRequest?,
-        error: WebResourceError?,
-    ) {
-        super.onReceivedError(view, request, error)
-
-        if (request?.isForMainFrame == true) {
-            Log.e(TAG, "Received error while loading ${request.url}: ${error?.description}")
-        }
-    }
-
-    override fun onReceivedHttpError(
-        view: WebView?,
-        request: WebResourceRequest?,
-        errorResponse: WebResourceResponse?,
-    ) {
-        super.onReceivedHttpError(view, request, errorResponse)
-
-        if (request?.isForMainFrame == true) {
-            val url = request.url
-            val statusCode = errorResponse?.statusCode
-            val reason = errorResponse?.reasonPhrase
-
-            Log.e(
-                TAG,
-                "Received HTTP error while loading $url ($statusCode): $reason",
-            )
-        }
-    }
-
-    override fun shouldOverrideUrlLoading(
-        view: WebView?,
-        request: WebResourceRequest?,
-    ): Boolean {
-        super.shouldOverrideUrlLoading(view, request)
-        // Follow redirects.
-        return false
-    }
-
-    private companion object {
-        private const val TAG = "CloudflareWebViewClient"
-    }
 }
---
+++ app/src/main/kotlin/com/prajwalch/torrentsearch/ui/settings/searchproviders/cloudflare/component/CloudflareWebViewClient.kt
@@ -0,0 +1,60 @@
+package com.prajwalch.torrentsearch.ui.settings.searchproviders.cloudflare.component
+
+import android.util.Log
+
+import android.webkit.WebResourceRequest
+import android.webkit.WebResourceResponse
+import android.webkit.WebView
+import android.webkit.WebViewClient
+
+import com.prajwalch.torrentsearch.network.HttpClient
+
+class CloudflareWebViewClient(
+    private val onChallengeSolved: () -> Unit,
+) : WebViewClient() {
+    private var challengeFound = false
+
+    override fun onPageFinished(view: WebView?, url: String) {
+        super.onPageFinished(view, url)
+        Log.i(TAG, "Finished loading $url")
+
+        if (!challengeFound) {
+            Log.i(TAG, "Challenge not found; Aborting")
+            onChallengeSolved()
+            return
+        }
+
+        val challengeSolved = HttpClient.getCookie(url).let {
+            it != null && it.contains("cf_clearance")
+        }
+        if (challengeSolved) {
+            Log.i(TAG, "Found the cookie")
+            onChallengeSolved()
+        } else {
+            Log.w(TAG, "Cookie not found")
+        }
+    }
+
+    override fun onReceivedHttpError(
+        view: WebView?,
+        request: WebResourceRequest?,
+        errorResponse: WebResourceResponse?,
+    ) {
+        super.onReceivedHttpError(view, request, errorResponse)
+
+        val isErrorStatus = errorResponse?.statusCode in ERROR_STATUS_CODES
+        val challengeHeader = errorResponse?.responseHeaders?.containsKey("cf_mitigated")
+
+        if (request?.isForMainFrame == true &&
+            (challengeHeader != null || isErrorStatus)
+        ) {
+            Log.i(TAG, "Found challenge page")
+            challengeFound = true
+        }
+    }
+
+    private companion object {
+        private const val TAG = "CloudflareWebViewClient"
+        private val ERROR_STATUS_CODES = listOf(403, 503)
+    }
+}
