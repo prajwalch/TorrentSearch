e79dd33f67f71b6faadfbf1f94273134a1cebc97
prajwalch <prajwal.chapagain58@gmail.com>
2026-06-21 06:32:11 +0545

    feat(SettingsScreen): Polish ExpandableItem implementation
    Signed-off-by: prajwalch <prajwal.chapagain58@gmail.com>

--- app/src/main/kotlin/com/prajwalch/torrentsearch/ui/settings/SettingsScreen.kt
+++ app/src/main/kotlin/com/prajwalch/torrentsearch/ui/settings/SettingsScreen.kt
@@ -11,19 +11,14 @@ import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.annotation.RequiresApi
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ListItem
 import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Switch
@@ -36,14 +31,12 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalUriHandler
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.unit.dp
 import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 
@@ -193,23 +186,16 @@ private fun AppearanceSettings(
             icon = R.drawable.ic_dark_mode,
         ) {
             DarkTheme.entries.forEach { theme ->
-                Row(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .clickable { onSetDarkTheme(theme) }
-                        .padding(horizontal = 16.dp, vertical = 12.dp),
-                    verticalAlignment = Alignment.CenterVertically,
-                ) {
-                    RadioButton(
-                        selected = uiState.darkTheme == theme,
-                        onClick = { onSetDarkTheme(theme) },
-                    )
-                    Spacer(modifier = Modifier.width(16.dp))
-                    Text(
-                        text = darkThemeStringResource(theme),
-                        style = MaterialTheme.typography.bodyMedium,
-                    )
-                }
+                ListItem(
+                    modifier = Modifier.clickable { onSetDarkTheme(theme) },
+                    leadingContent = {
+                        RadioButton(
+                            selected = uiState.darkTheme == theme,
+                            onClick = { onSetDarkTheme(theme) },
+                        )
+                    },
+                    headlineContent = { Text(darkThemeStringResource(theme)) },
+                )
             }
         }
 
--- app/src/main/kotlin/com/prajwalch/torrentsearch/ui/settings/component/ExpandableItem.kt
+++ app/src/main/kotlin/com/prajwalch/torrentsearch/ui/settings/component/ExpandableItem.kt
@@ -3,30 +3,19 @@ package com.prajwalch.torrentsearch.ui.settings.component
 import androidx.annotation.DrawableRes
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.core.animateFloatAsState
-import androidx.compose.animation.expandVertically
-import androidx.compose.animation.fadeIn
-import androidx.compose.animation.fadeOut
-import androidx.compose.animation.shrinkVertically
-import androidx.compose.animation.slideInVertically
-import androidx.compose.animation.slideOutVertically
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ListItem
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.unit.dp
+
 import com.prajwalch.torrentsearch.R
 
 @Composable
@@ -36,56 +25,37 @@ fun ExpandableItem(
     isExpanded: Boolean,
     onToggle: () -> Unit,
     @DrawableRes icon: Int? = null,
-    content: @Composable () -> Unit
+    content: @Composable ColumnScope.() -> Unit,
 ) {
-    val rotation by animateFloatAsState(targetValue = if (isExpanded) 180f else 0f, label = "rotation")
-    
+    val trailingIconRotation by animateFloatAsState(
+        targetValue = if (isExpanded) 180f else 0f,
+        label = "rotation"
+    )
+
     Column(modifier = Modifier.fillMaxWidth()) {
-        Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .clickable(onClick = onToggle)
-                .padding(16.dp),
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            if (icon != null) {
+        ListItem(
+            modifier = Modifier.clickable(onClick = onToggle),
+            leadingContent = icon?.let { iconId ->
+                {
+                    Icon(
+                        painter = painterResource(iconId),
+                        contentDescription = null,
+                    )
+                }
+            },
+            headlineContent = { Text(title) },
+            trailingContent = {
                 Icon(
-                    painter = painterResource(id = icon),
+                    modifier = Modifier.rotate(trailingIconRotation),
+                    painter = painterResource(R.drawable.ic_keyboard_arrow_down),
                     contentDescription = null,
-                    modifier = Modifier.size(24.dp),
-                    tint = MaterialTheme.colorScheme.onSurfaceVariant
                 )
-                Spacer(modifier = Modifier.width(16.dp))
-            }
-            Column(
-                modifier = Modifier.weight(1f)
-            ) {
-                Text(
-                    text = title,
-                    style = MaterialTheme.typography.bodyLarge
-                )
-                Text(
-                    text = subtitle,
-                    style = MaterialTheme.typography.bodySmall,
-                    color = MaterialTheme.colorScheme.onSurfaceVariant
-                )
-            }
-            Icon(
-                painter = painterResource(id = R.drawable.ic_keyboard_arrow_down),
-                contentDescription = null,
-                tint = MaterialTheme.colorScheme.onSurfaceVariant,
-                modifier = Modifier.rotate(rotation)
-            )
-        }
-        
-        AnimatedVisibility(
-            visible = isExpanded,
-            enter = slideInVertically(initialOffsetY = { -it / 2 }) + expandVertically() + fadeIn(),
-            exit = slideOutVertically(targetOffsetY = { -it / 2 }) + shrinkVertically() + fadeOut()
-        ) {
-            Column(modifier = Modifier.fillMaxWidth()) {
-                content()
-            }
+            },
+            supportingContent = { Text(subtitle) },
+        )
+
+        AnimatedVisibility(visible = isExpanded) {
+            Column(modifier = Modifier.fillMaxWidth(), content = content)
         }
     }
 }
