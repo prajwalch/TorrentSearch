608ffc9a55a201998349b1bd09fbc9e6142bb3b7
prajwalch <prajwal.chapagain58@gmail.com>
2026-06-29 10:27:33 +0545

    chore(TorrentSearchConstants): Improve doc comments
    Signed-off-by: prajwalch <prajwal.chapagain58@gmail.com>

--- app/src/main/kotlin/com/prajwalch/torrentsearch/constant/TorrentSearchConstants.kt
+++ app/src/main/kotlin/com/prajwalch/torrentsearch/constant/TorrentSearchConstants.kt
@@ -3,20 +3,20 @@ package com.prajwalch.torrentsearch.constant
 import com.prajwalch.torrentsearch.BuildConfig
 
 object TorrentSearchConstants {
-    /** Github repository URL. */
+    /** GitHub repository URL. */
     const val GITHUB_REPO_URL = "https://github.com/prajwalch/TorrentSearch"
 
-    /** Github latest release page URL. */
+    /** GitHub latest release page URL. */
     const val GITHUB_RELEASE_URL = "$GITHUB_REPO_URL/releases/tag/v${BuildConfig.VERSION_NAME}"
 
-    /** Github wiki URL.*/
+    /** GitHub wiki URL.*/
     const val GITHUB_WIKI_URL = "$GITHUB_REPO_URL/wiki"
 
     /** Torzanb search provider configuration guide wiki URL. */
     const val TORZNAB_HOW_TO_ADD_WIKI =
         "$GITHUB_WIKI_URL/How-to-add-and-configure-Torznab-search-provider"
 
-    /** MIME type of a logs file. */
+    /** MIME type of logs file. */
     const val LOGS_FILE_TYPE = "text/plain"
 
     /** Name of the export file. */
@@ -28,6 +28,6 @@ object TorrentSearchConstants {
     /** Name of a crash logs file. */
     const val CRASH_LOGS_FILE_NAME = "torrentsearch_crash_logs.txt"
 
-    /** Name a application main logs file. */
+    /** Name of an application main logs file. */
     const val APP_LOGS_FILE_NAME = "torrentsearch_logs.txt"
 }
