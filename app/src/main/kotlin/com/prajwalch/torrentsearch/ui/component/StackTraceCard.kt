6b537aa443c7229cf0119f7fa64f2075a2049d27
prajwalch <prajwal.chapagain58@gmail.com>
2026-08-19 18:37:22 +0545

    feat(StackTraceCard): Use Monospace font family for the stack trace text
    Signed-off-by: prajwalch <prajwal.chapagain58@gmail.com>

--- app/src/main/kotlin/com/prajwalch/torrentsearch/ui/component/StackTraceCard.kt
+++ app/src/main/kotlin/com/prajwalch/torrentsearch/ui/component/StackTraceCard.kt
@@ -14,6 +14,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.text.font.FontFamily
 
 import com.prajwalch.torrentsearch.ui.theme.spaces
 
@@ -37,7 +38,9 @@ fun StackTraceCard(
                 .padding(contentPadding)
                 .verticalScroll(state = rememberScrollState()),
         ) {
-            SelectionContainer { Text(text = stackTrace) }
+            SelectionContainer {
+                Text(text = stackTrace, fontFamily = FontFamily.Monospace)
+            }
         }
     }
 }
