8ea1bad43d88f9550787229821770b74c85bdfcb
prajwalch <prajwal.chapagain58@gmail.com>
2026-09-21 14:01:10 +0545

    feat(OutlinedUrlTextField): Set text field type as URI
    Signed-off-by: prajwalch <prajwal.chapagain58@gmail.com>

--- app/src/main/kotlin/com/prajwalch/torrentsearch/ui/searchproviders/addedit/component/OutlinedUrlTextField.kt
+++ app/src/main/kotlin/com/prajwalch/torrentsearch/ui/searchproviders/addedit/component/OutlinedUrlTextField.kt
@@ -1,5 +1,6 @@
 package com.prajwalch.torrentsearch.ui.searchproviders.addedit.component
 
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Icon
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
@@ -7,6 +8,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
 
 import com.prajwalch.torrentsearch.R
 
@@ -45,5 +47,6 @@ fun OutlinedUrlTextField(
         supportingText = supportingText,
         isError = isError,
         singleLine = true,
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Uri),
     )
 }
